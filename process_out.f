5b6bcf27d133491f0bcae097a16263c24c698b15
Matthew West <mwest@illinois.edu>
2005-10-15 03:26:20 +0000

    more ftnchek warning fixes

--- process_out.f
+++ process_out.f
@@ -31,7 +31,7 @@ CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
 
       ! check there is exactly one commandline argument
       if (iargc() .ne. 1) then
-         write(6,*)'Usage: process_out <filename.d>'
+         write(6,*) 'Usage: process_out <filename.d>'
          call exit(2)
       endif
 
@@ -39,12 +39,12 @@ CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
       call getarg(1, name_in)
       i = len_trim(name_in)
       if (i .gt. 40) then
-         write(6,*)'ERROR: filename too long'
+         write(6,*) 'ERROR: filename too long'
          call exit(2)
       endif
       if ((name_in(i:i) .ne. 'd') .or.
      &     (name_in((i-1):(i-1)) .ne. '.')) then
-         write(6,*)'ERROR: Filename must end in .d'
+         write(6,*) 'ERROR: Filename must end in .d'
          call exit(2)
       endif
 
@@ -58,11 +58,11 @@ CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
       name_out_num_avg((i-1):) = '_num_avg.d'
       name_out_mass_avg((i-1):) = '_mass_avg.d'
 
-      write(6,*)'name_in = ', name_in
-      write(6,*)'name_out_num = ', name_out_num
-      write(6,*)'name_out_mass = ', name_out_mass
-      write(6,*)'name_out_num_avg = ', name_out_num_avg
-      write(6,*)'name_out_mass_avg = ', name_out_mass_avg
+      write(6,*) 'name_in = ', name_in
+      write(6,*) 'name_out_num = ', name_out_num
+      write(6,*) 'name_out_mass = ', name_out_mass
+      write(6,*) 'name_out_num_avg = ', name_out_num_avg
+      write(6,*) 'name_out_mass_avg = ', name_out_mass_avg
 
       ! open files
       open(f_in, file=name_in)
@@ -72,36 +72,36 @@ CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
       open(f_out_mass_avg, file=name_out_mass_avg)
 
       ! read and check dimensions
-      read(f_in, '(a10,i10)'), dum, n_loop
-      read(f_in, '(a10,i10)'), dum, n_bin
-      read(f_in, '(a10,i10)'), dum, n_time
+      read(f_in, '(a10,i10)') dum, n_loop
+      read(f_in, '(a10,i10)') dum, n_bin
+      read(f_in, '(a10,i10)') dum, n_time
 
       if (n_loop .gt. n_loop_max) then
-         write(6,*)'ERROR: n_loop too large'
+         write(6,*) 'ERROR: n_loop too large'
          call exit(2)
       endif
       if (n_bin .gt. n_bin_max) then
-         write(6,*)'ERROR: n_bin too large'
+         write(6,*) 'ERROR: n_bin too large'
          call exit(2)
       endif
       if (n_time .gt. n_time_max) then
-         write(6,*)'ERROR: n_time too large'
+         write(6,*) 'ERROR: n_time too large'
          call exit(2)
       endif
 
-      write(6,*)'n_loop = ', n_loop
-      write(6,*)'n_bin = ', n_bin
-      write(6,*)'n_time = ', n_time
+      write(6,*) 'n_loop = ', n_loop
+      write(6,*) 'n_bin = ', n_bin
+      write(6,*) 'n_time = ', n_time
 
-      write(n_loop_str, '(i10)'), n_loop
-      write(n_time_str, '(i10)'), n_time
+      write(n_loop_str, '(i10)') n_loop
+      write(n_time_str, '(i10)') n_time
 
       ! read all data
       do i_loop = 1,n_loop
          do i_time = 1,n_time
-            read(f_in, '(a10,e14.5)'), dum, time
+            read(f_in, '(a10,e14.5)') dum, time
             do i_bin = 1,n_bin
-               read(f_in, '(i8,3e14.5)'), i, rr(i_bin),
+               read(f_in, '(i8,3e14.5)') i, rr(i_bin),
      &              n(i_loop, i_time, i_bin), g(i_loop, i_time, i_bin)
             enddo
          enddo
@@ -110,8 +110,8 @@ CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
       ! compute averages
       do i_time = 1,n_time
          do i_bin = 1,n_bin
-            g_avg(i_time, i_bin) = 0.
-            n_avg(i_time, i_bin) = 0.
+            g_avg(i_time, i_bin) = 0d0
+            n_avg(i_time, i_bin) = 0d0
             do i_loop = 1,n_loop
                g_avg(i_time, i_bin) = g_avg(i_time, i_bin)
      &              + g(i_loop, i_time, i_bin)
@@ -125,97 +125,27 @@ CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
 
       ! output data
       do i_time = 1,n_time
-         write(f_out_num, '(//,a10,i10)'), 'time', i_time - 1
-         write(f_out_mass, '(//,a10,i10)'), 'time', i_time - 1
+         write(f_out_num, '(//,a10,i10)') 'time', i_time - 1
+         write(f_out_mass, '(//,a10,i10)') 'time', i_time - 1
          do i_bin = 1,n_bin
-            write(f_out_num, '(i8,e14.5,'//n_loop_str//'e14.5)'),
+            write(f_out_num, '(i8,e14.5,'//n_loop_str//'e14.5)')
      &           i_bin, rr(i_bin),
      &           (n(i_loop, i_time, i_bin), i_loop = 1,n_loop)
-            write(f_out_mass, '(i8,e14.5,'//n_loop_str//'e14.5)'),
+            write(f_out_mass, '(i8,e14.5,'//n_loop_str//'e14.5)')
      &           i_bin, rr(i_bin),
      &           (g(i_loop, i_time, i_bin), i_loop = 1,n_loop)
          enddo
       enddo
 
       do i_bin = 1,n_bin
-         write(f_out_num_avg, '(i8,e14.5,'//n_time_str//'e14.5)'),
+         write(f_out_num_avg, '(i8,e14.5,'//n_time_str//'e14.5)')
      &        i_bin, rr(i_bin),
      &        (n_avg(i_time, i_bin), i_time = 1,n_time)
-         write(f_out_mass_avg, '(i8,e14.5,'//n_time_str//'e14.5)'),
+         write(f_out_mass_avg, '(i8,e14.5,'//n_time_str//'e14.5)')
      &        i_bin, rr(i_bin),
      &        (g_avg(i_time, i_bin), i_time = 1,n_time)
       enddo
       
-
-c       integer n_bin,i_loop,tmax
-c       parameter (n_bin=160,i_loop=1,tmax=11)
-c
-c       real*8 eta(n_bin),g(i_loop,tmax,n_bin),n(i_loop,tmax,n_bin)
-c       real*8 g_bar(tmax,n_bin),n_bar(tmax,n_bin)
-c       integer k, l, i, mm
-c       character*20 dum
-c
-c       open(20,file='mc_fix.d')
-c       open(30,file='mc_n.dtest2')
-c       open(40,file='mc_m.dtest2')
-c       open(50,file='mcr_n.dtest2')
-c       open(60,file='mcr_m.dtest2')
-c      
-c
-c       do k=1,tmax
-c          do l=1,n_bin
-c             n_bar(k,l) = 0.
-c             g_bar(k,l) = 0.
-c          enddo
-c       enddo
-c
-c       do i=1,i_loop
-c
-c          read(20,'(a)')dum
-c          write(6,'(a)')dum
-c          do k=1,tmax
-c             read(20,'(a)')dum
-c             write(6,'(a)')dum
-c                 do l=1,n_bin
-c                    read(20,'(i4,5e14.5)')
-c     *                      mm,eta(l),n(i,k,l),g(i,k,l)
-c                    write(6,*)mm,eta(l),n(i,k,l),g(i,k,l)
-c                  enddo
-c           enddo 
-c        enddo
-c
-c       do k=1,tmax
-c          do l=1,n_bin
-c             do i=1,i_loop
-c                    g_bar(k,l) = g_bar(k,l)+g(i,k,l)
-c                    n_bar(k,l) = n_bar(k,l)+n(i,k,l)
-c             enddo
-c          
-c             g_bar(k,l) = g_bar(k,l)/i_loop
-c             n_bar(k,l) = n_bar(k,l)/i_loop
-c          enddo
-c        enddo
-c
-c        do k=1,tmax
-c           write(50,*)'time= ',k-1
-c           write(60,*)'time= ',k-1
-c       
-c           do l=1,n_bin
-c              write(50,'(i4,100e14.5)')l,eta(l),(n(i,k,l),i=1,i_loop),
-c     &                 n_bar(k,l)
-c              write(60,'(i4,100e14.5)')l,eta(l),(g(i,k,l),i=1,i_loop),
-c     &                 g_bar(k,l)
-c           enddo
-c        enddo
-c 
-c       do l=1,n_bin
-c            write(30,'(i4,100e14.5)')l,eta(l)*1.e+06
-c     &           ,(n_bar(k,l),k=1,tmax)
-c            write(40,'(i4,100e14.5)')l,eta(l)*1.e+06
-c     &           ,(g_bar(k,l)*1000.,k=1,tmax)
-c       enddo
-
-
-        end
-       
+      end
+      
 CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
--- run_golovin_adapt.f
+++ run_golovin_adapt.f
@@ -7,18 +7,18 @@ CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
       integer MM, n_bin, n_loop, scal
       real*8 t_max, rho_p, N_0, t_print, p_max, V_0
       real*8 r_samp_max, del_t_max
-      parameter (MM = 10000)         ! number of particles
-      parameter (n_bin = 160)        ! number of bins
-      parameter (n_loop = 1)         ! number of loops
-      parameter (scal = 3)           ! scale factor for bins
-      parameter (t_max = 600.)       ! total simulation time (seconds)
-      parameter (rho_p = 1000.)      ! particle density (kg/m^3)
-      parameter (N_0 = 1d9)          ! particle number concentration (#/m^3)
-      parameter (t_print = 60.)      ! interval between printing (s)
-      parameter (p_max = 0.01)       ! maximum coagulation probability
-      parameter (r_samp_max = 0.005) ! maximum sampling ratio per timestep
-      parameter (del_t_max = 1.)     ! maximum timestep
-      parameter (V_0 = 4.1886e-15)   !
+      parameter (MM = 10000)           ! number of particles
+      parameter (n_bin = 160)          ! number of bins
+      parameter (n_loop = 1)           ! number of loops
+      parameter (scal = 3)             ! scale factor for bins
+      parameter (t_max = 600d0)        ! total simulation time (seconds)
+      parameter (rho_p = 1000d0)       ! particle density (kg/m^3)
+      parameter (N_0 = 1d9)            ! particle number concentration (#/m^3)
+      parameter (t_print = 60d0)       ! interval between printing (s)
+      parameter (p_max = 0.01d0)       ! maximum coagulation probability
+      parameter (r_samp_max = 0.005d0) ! maximum sampling ratio per timestep
+      parameter (del_t_max = 1d0)      ! maximum timestep
+      parameter (V_0 = 4.1886d-15)     !
 
       integer M, M_comp, i_loop, k
       real*8 V(MM), V_comp, dlnr
@@ -26,12 +26,12 @@ CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
       real*8 g(n_bin), n_ln(n_bin)
 
       real*8 pi
-      parameter (pi = 3.14159265358979323846)
+      parameter (pi = 3.14159265358979323846d0)
 
       external kernel_golovin
 
       open(30,file='out_golovin_adapt.d')
-      call print_header(n_loop, n_bin, nint(t_max / t_print + 1.))
+      call print_header(n_loop, n_bin, nint(t_max / t_print + 1d0))
       call srand(10)
 
       do i_loop = 1,n_loop
@@ -43,7 +43,7 @@ CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
          
          ! define initial exponential distribution
          do k = 1,n_bin
-            n_ini(k) = pi/2.0 * dp(k)**3.0 * M/V_0 * exp(-(vv(k)/V_0))
+            n_ini(k) = pi/2d0 * dp(k)**3 * M/V_0 * exp(-(vv(k) / V_0))
          enddo
 
          call compute_volumes(n_bin, MM, n_ini, dp, dlnr, V)

d338c0d598dc26b583a2118ca737495e7bc5b2b8
Nicole Riemer <nriemer@illinois.edu>
2007-01-29 17:06:58 +0000

    corrected for factor 1000 in kernel_golovin.f g_bin of analytical solution now really is the volume, not the mass
    git-svn-id: https://lagrange.mechse.illinois.edu/svn/particle_mc/code/trunk@1417 3149b016-92da-452d-8456-5fca86d938a0

--- kernel_golovin.f
+++ kernel_golovin.f
@@ -80,7 +80,7 @@ contains
     endif
     
     do k = 1,n_bin
-       bin_g(k) = pi/6d0 * rho_p * (2d0*vol2rad(bin_v(k)))**3 * dble(bin_n(k))
+       bin_g(k) = pi/6d0 * (2d0*vol2rad(bin_v(k)))**3 * dble(bin_n(k))
     enddo
     
     do k = 1,n_bin

02e8b10e3d04a1a2ae58bdcacdb5beb6675c934d
Matthew West <mwest@illinois.edu>
2007-06-06 05:47:35 +0000

    mosaic test-case now runs, but segfaults

--- test/mosaic.sh
+++ test/mosaic.sh
@@ -2,65 +2,13 @@
 
 cat <<ENDINFO
 
-Golovin Test-case
------------------
+MOSAIC Test-case
+----------------
 
-This tests the Monte Carlo coagulation code for the Golovin kernel
-with an exponential initial condition for which an analytical solution
-is known. The Monte Carlo code is looped and average to reduce
-error. To improve the quality of the results either the number of
-loops or the number of particles can be increased.
-
-This test-case demonstrates generating exact solutions and using
-repeated loops of the Monte Carlo code to reduce the error, and serves
-as model verification against an analytical solution.
+This tests the interface to the MOSAIC chemistry code.
 
 ENDINFO
 sleep 1
 
-echo ../src/partmc golovin_mc.spec
-../src/partmc golovin_mc.spec
-echo ../src/process_out out/out_golovin_mc.d
-../src/process_out out/out_golovin_mc.d
-
-echo ../src/partmc golovin_exact.spec
-../src/partmc golovin_exact.spec
-echo ../src/process_out out/out_golovin_exact.d
-../src/process_out out/out_golovin_exact.d
-
-echo Plotting number density
-gnuplot -persist <<ENDNUM
-set logscale
-set xlabel "radius (m)"
-set ylabel "number density (#/m^3)
-set title "Golovin kernel, exponential initial condition"
-plot [1e-7:1e-3] [1e4:1e10] "out/out_golovin_mc_num_avg.d" using 2:3 title "Monte Carlo (0 mins)"
-replot "out/out_golovin_mc_num_avg.d" using 2:8 title "Monte Carlo (5 mins)"
-replot "out/out_golovin_mc_num_avg.d" using 2:13 title "Monte Carlo (10 mins)"
-replot "out/out_golovin_exact_num_avg.d" using 2:3 w l title "Analytical (0 mins)"
-replot "out/out_golovin_exact_num_avg.d" using 2:8 w l title "Analytical (5 mins)"
-replot "out/out_golovin_exact_num_avg.d" using 2:13 w l title "Analytical (10 mins)"
-set terminal postscript eps
-set output "out/plot_golovin_num.eps"
-replot
-ENDNUM
-epstopdf out/plot_golovin_num.eps
-
-echo Plotting volume density
-gnuplot -persist <<ENDVOL
-set logscale
-set xlabel "radius (m)"
-set ylabel "volume density (m^3/m^3)"
-set title "Golovin kernel, exponential initial condition"
-set key left top
-plot [1e-7:1e-3] [1e-14:1e-4] "out/out_golovin_mc_vol_avg.d" using 2:3 title "Monte Carlo (0 mins)"
-replot "out/out_golovin_mc_vol_avg.d" using 2:8 title "Monte Carlo (5 mins)"
-replot "out/out_golovin_mc_vol_avg.d" using 2:13 title "Monte Carlo (10 mins)"
-replot "out/out_golovin_exact_vol_avg.d" using 2:3 w l title "Analytical (0 mins)"
-replot "out/out_golovin_exact_vol_avg.d" using 2:8 w l title "Analytical (5 mins)"
-replot "out/out_golovin_exact_vol_avg.d" using 2:13 w l title "Analytical (10 mins)"
-set terminal postscript eps
-set output "out/plot_golovin_vol.eps"
-replot
-ENDVOL
-epstopdf out/plot_golovin_vol.eps
+echo ../src/partmc mosaic.spec
+../src/partmc mosaic.spec
--- test/mosaic.spec
+++ test/mosaic.spec
@@ -1,5 +1,6 @@
 run_type mc                     # Monte Carlo run
-output_file out/out_mosaic.d # name of output file
+output_file out/mosaic_out.d    # name of output file
+state_prefix out/mosaic_state   # prefix of state files
 n_loop 1                        # number of Monte Carlo loops
 num_conc 1d9                    # particle concentration (#/m^3)
 kernel golovin                  # coagulation kernel
@@ -10,7 +11,7 @@ t_output 60                     # output interval (0 disables) (s)
 t_state 0                       # state output interval (0 disables) (s)
 t_progress 60                   # progress printing interval (0 disables) (s)
 
-temp_profile temps.dat          # temperature profile file
+temp_profile temp_constant_15C.dat # temperature profile file
 RH 0.999                        # initial relative humidity (1)
 pressure 1d5                    # initial pressure (Pa)
 rho_a 1.25                      # initial air density (kg/m^3)
@@ -20,13 +21,13 @@ altitude 0                      # altitude (m)
 start_time 0                    # start time (s since 00:00 UTC)
 start_day 1                     # start day of year (UTC)
 
-gas_init_conc gas_init.dat      # initial gas concentrations
-aerosol_data aerosol.dat        # file containing aerosol data
+gas_init_conc gas_init_mosaic.dat # initial gas concentrations
+aerosol_data aerosol_mosaic.dat # file containing aerosol data
 
 n_init_dist 1                   # number of initial aerosol distributions
 
-n_p 10000                       # number of particles
-vol_frac aero_dist1.dat         # composition proportions of species
+n_p 10                          # number of particles
+vol_frac comp_mosaic.dat        # composition proportions of species
 dist_type exp                   # type of distribution
 dist_mean_vol 4.1886d-15        # mean volume (m^3)
 

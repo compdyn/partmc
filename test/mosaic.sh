c214beef975c82048a4f0ea9bd0812ce5a80cdda
Nicole Riemer <nriemer@illinois.edu>
2007-07-14 15:29:56 +0000

    mosaic test case edit

--- test/mosaic.sh
+++ test/mosaic.sh
@@ -21,10 +21,9 @@ set xlabel "time (s)"
 set ylabel "gas concentration (ppb)
 set y2label "aerosol number density (#/m^3)
 set title "MOSAIC test case"
-#plot "out/mosaic_summary_gas.d" using 1:7 with lines axes x1y1 title "gas NO2"
-#replot "out/mosaic_summary_gas.d" using 1:12 with lines axes x1y1 title "gas O3"
-plot "out/mosaic_summary_gas.d" using 1:2 with lines axes x1y1 title "gas H2SO4"
-#replot "out/mosaic_summary_aero_total.d" using 1:4 with lines axes x1y2 title "aerosol SO4"
+plot "out/mosaic_summary_gas.d" using 1:7 with lines axes x1y1 title "gas NO2"
+replot "out/mosaic_summary_gas.d" using 1:12 with lines axes x1y1 title "gas O3"
+replot "out/mosaic_summary_aero_total.d" using 1:4 with lines axes x1y2 title "aerosol SO4"
 set terminal postscript eps
 set output "out/mosaic_plot_history.eps"
 replot

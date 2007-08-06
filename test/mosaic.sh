d13f685f84441a9c02d1b70cff7b0c1c6fcacce5
Matthew West <mwest@illinois.edu>
2007-08-06 21:23:52 +0000

    mosaic testcase really working

--- test/mosaic.sh
+++ test/mosaic.sh
@@ -19,12 +19,15 @@ echo Plotting time history of gas and aerosol
 gnuplot -persist <<ENDHISTORY
 set xlabel "time (s)"
 set ylabel "gas concentration (ppb)
-set y2label "aerosol number density (#/m^3)
+set y2label "aerosol volume density (#/m^3)
+set y2tics
 set title "MOSAIC test case"
-#plot "out/mosaic_summary_gas.d" using 1:7 with lines axes x1y1 title "gas NO2"
-#replot "out/mosaic_summary_gas.d" using 1:12 with lines axes x1y1 title "gas O3"
-#plot "out/mosaic_summary_gas.d" using 1:5 with lines axes x1y1 title "gas NH3"
-plot "out/mosaic_summary_aero_total.d" using 1:4 with lines axes x1y2 title "aerosol SO4"
+plot "out/mosaic_summary_gas.d" using 1:7 with lines axes x1y1 title "gas NO2"
+replot "out/mosaic_summary_gas.d" using 1:12 with lines axes x1y1 title "gas O3"
+replot "out/mosaic_summary_gas.d" using 1:5 with lines axes x1y1 title "gas NH3"
+replot "out/mosaic_summary_aero_total.d" using 1:4 with lines axes x1y2 title "aerosol SO4"
+replot "out/mosaic_summary_aero_total.d" using 1:5 with lines axes x1y2 title "aerosol NO3"
+replot "out/mosaic_summary_aero_total.d" using 1:7 with lines axes x1y2 title "aerosol NH4"
 set terminal postscript eps
 set output "out/mosaic_plot_history.eps"
 replot

c816fd9dd829c072ec548dc8b2919751047da4b4
Nicole Riemer <nriemer@illinois.edu>
2007-08-08 19:08:43 +0000

    monodisperse case

--- urban_plume/urban_plume.sh
+++ urban_plume/urban_plume.sh
@@ -12,9 +12,9 @@ ENDINFO
 sleep 1
 
 echo ../src/partmc urban_plume.spec
-../src/partmc urban_plume.spec
+#../src/partmc urban_plume.spec
 echo ../src/process_summary out/urban_plume_summary.d
-../src/process_summary out/urban_plume_summary.d
+#../src/process_summary out/urban_plume_summary.d
 
 echo Plotting time history of gas and aerosol
 gnuplot -persist <<ENDHISTORY
@@ -24,13 +24,20 @@ set y2label "aerosol volume density (#/m^3)
 set y2tics
 set title "URBAN_PLUME test case"
 #plot "out/urban_plume_summary_aero_total.d" using (\$1/3600):2 with lines axes x1y1 title "number dens."
-#replot "out/urban_plume_summary_aero_total.d" using (\$1/3600):4 with lines axes x1y2 title "aerosol SO4 "
-plot "out/urban_plume_summary_gas.d" using (\$1/3600):5 with lines axes x1y1 title "gas NH3"
-replot "out/urban_plume_summary_gas.d" using (\$1/3600):2 with lines axes x1y1 title "gas H2SO4"
-replot "out/urban_plume_summary_gas.d" using (\$1/3600):19 with lines axes x1y1 title "gas SO2"
+#plot "out/urban_plume_summary_aero_total.d" using (\$1/3600):4 with lines axes x1y2 title "aerosol SO4 "
+#replot "out/urban_plume_summary_gas.d" using (\$1/3600):5 with lines axes x1y1 title "gas NH3"
+#replot "out/urban_plume_summary_gas.d" using (\$1/3600):2 with lines axes x1y1 title "gas H2SO4"
+#plot "out/urban_plume_summary_gas.d" using (\$1/3600):34 with lines axes x1y1 title "gas ARO1"
+#plot "out/urban_plume_summary_gas.d" using (\$1/3600):12 with lines axes x1y1 title "gas O3"
+#plot "out/urban_plume_summary_gas.d" using (\$1/3600):6 with lines axes x1y1 title "gas NO"
+#replot "out/urban_plume_summary_gas.d" using (\$1/3600):7 with lines axes x1y1 title "gas NO2"
+#replot "out/urban_plume_summary_gas.d" using (\$1/3600):24 with lines axes x1y1 title "gas HCHO"
 #replot "out/urban_plume_summary_aero_total.d" using (\$1/3600):7 with lines axes x1y2 title "aerosol NH4"
 #replot "out/urban_plume_summary_aero_total.d" using (\$1/3600):5 with lines axes x1y2 title "aerosol NO3"
-#replot "out/urban_plume_summary_aero_total.d" using (\$1/3600):7 with lines axes x1y2 title "aerosol NH4"
+plot "out/urban_plume_summary_aero_total.d" using (\$1/3600):12 with lines axes x1y2 title "aerosol OC"
+replot "out/urban_plume_summary_aero_total.d" using (\$1/3600):13 with lines axes x1y2 title "aerosol EC"
+#plot "out/urban_plume_summary_aero_total.d" using (\$1/3600):15 with lines axes x1y2 title "aerosol ARO1"
+#replot "out/urban_plume_summary_aero_total.d" using (\$1/3600):16 with lines axes x1y2 title "aerosol ARO2"
 set terminal postscript eps
 set output "out/urban_plume_plot_history.eps"
 replot

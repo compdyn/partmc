11b94af4eabb4b0dec617f8b749226bea929770c
Matthew West <mwest@illinois.edu>
2007-09-23 14:35:50 +0000

    test/poisson using new plotting

--- test/poisson.sh
+++ test/poisson.sh
@@ -17,36 +17,15 @@ sleep 1
 
 echo ./poisson_sample 1 50 10000000
 ./poisson_sample 1 50 10000000 > out/poisson_1.d
-
 echo ./poisson_sample 4 50 10000000
 ./poisson_sample 4 50 10000000 > out/poisson_4.d
-
 echo ./poisson_sample 10 50 10000000
 ./poisson_sample 10 50 10000000 > out/poisson_10.d
-
 echo ./poisson_sample 20 50 10000000
 ./poisson_sample 20 50 10000000 > out/poisson_20.d
-
 echo ./poisson_sample 30 50 10000000
 ./poisson_sample 30 50 10000000 > out/poisson_30.d
 
-echo Plotting Poisson samples
-gnuplot -persist <<ENDPOISSON
-set xlabel "n"
-set ylabel "Prob(n)"
-set title "Sampled and exact Poisson PDFs"
-plot "out/poisson_1.d" using 1:3 title "rate 1 (sampled)"
-replot "out/poisson_1.d" using 1:2 title "rate 1 (exact)" with lines
-replot "out/poisson_4.d" using 1:3 title "rate 4 (sampled)"
-replot "out/poisson_4.d" using 1:2 title "rate 4 (exact)" with lines
-replot "out/poisson_10.d" using 1:3 title "rate 10 (sampled)"
-replot "out/poisson_10.d" using 1:2 title "rate 10 (exact)" with lines
-replot "out/poisson_20.d" using 1:3 title "rate 20 (sampled)"
-replot "out/poisson_20.d" using 1:2 title "rate 20 (exact)" with lines
-replot "out/poisson_30.d" using 1:3 title "rate 30 (sampled)"
-replot "out/poisson_30.d" using 1:2 title "rate 30 (exact)" with lines
-set terminal postscript eps
-set output "out/poisson_plot.eps"
-replot
-ENDPOISSON
-epstopdf out/poisson_plot.eps
+echo ./poisson_plot.py
+./poisson_plot.py
+echo Now view out/poisson.pdf
---
+++ test/poisson_plot.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# Copyright (C) 2007 Matthew West
+# Licensed under the GNU General Public License version 2 or (at your
+# option) any later version. See the file COPYING for details.
+
+import os, sys
+import copy as module_copy
+sys.path.append("../tool")
+from pmc_data import *
+from pmc_pyx import *
+sys.path.append(os.path.expanduser("~/.python"))
+from pyx import *
+
+n_list = [1, 4, 10, 20, 30]
+
+grid_painter = graph.axis.painter.regular(gridattrs = [style.linestyle.dotted])
+
+g = graph.graphxy(width = 10,
+		  x = graph.axis.linear(title = "n",
+					painter = grid_painter),
+		  y = graph.axis.linear(title = "Prob(n)",
+					painter = grid_painter),
+		  key = graph.key.key(pos = "tr"))
+
+for i in range(len(n_list)):
+    g.plot(graph.data.file("out/poisson_%d.d" % n_list[i],
+			   x = 1, y = 2,
+			   title = "rate %d (exact)" % n_list[i]),
+	   styles = [graph.style.line(lineattrs = [color_list[i]])])
+    g.plot(graph.data.file("out/poisson_%d.d" % n_list[i],
+			   x = 1, y = 3,
+			   title = "rate %d (sampled)" % n_list[i]),
+	   styles = [graph.style.symbol(symbol = graph.style.symbol.circle,
+					size = 0.05,
+					symbolattrs = [color_list[i]])])
+
+g.writePDFfile("out/poisson")

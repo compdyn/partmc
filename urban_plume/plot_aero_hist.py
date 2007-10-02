7ab5030ca591c08ef3f1ae453d2f9a55d1f7e8b0
Matthew West <mwest@illinois.edu>
2007-10-02 07:55:22 +0000

    minor plotting fixes

--- urban_plume/plot_aero_hist.py
+++ urban_plume/plot_aero_hist.py
@@ -20,18 +20,20 @@ data = pmc_var(NetCDFFile("out/urban_plume_state_0001.nc"),
 		select("unit", "mass_den")])
 data.write_summary(sys.stdout)
 
+data.scale_dim("time", 1.0/3600)
+data.scale(1e6)
+
 g = graph.graphxy(
     width = 10,
     x = graph.axis.linear(title = "time (hour)",
 			  painter = grid_painter),
-    y = graph.axis.linear(title = "mass density (m$^3$/m$^3$)",
+    y = graph.axis.linear(title = "mass density ($\mu$g/m$^3$)",
 			  painter = grid_painter),
     key = graph.key.key(pos = "tr"))
 
 for i in range(len(aero_species)):
     data_slice = module_copy.deepcopy(data)
     data_slice.reduce([select("aero_species", aero_species[i])])
-    data_slice.scale_dim("time", 1.0/3600)
     g.plot(graph.data.list(data_slice.data_center_list(),
 			   x = 1, y = 2,
 			   title = tex_species(aero_species[i])),

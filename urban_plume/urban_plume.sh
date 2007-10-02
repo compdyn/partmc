63da4c6000cd60f1779136207eb575a6b615cc17
Matthew West <mwest@illinois.edu>
2007-10-02 07:36:21 +0000

    actually run the plot commands

--- urban_plume/urban_plume.sh
+++ urban_plume/urban_plume.sh
@@ -15,8 +15,13 @@ echo ../src/partmc urban_plume.spec
 ../src/partmc urban_plume.spec
 
 echo ./plot_aero_dist.py
+./plot_aero_dist.py
 echo ./plot_aero_hist.py
+./plot_aero_hist.py
 echo ./plot_gas.py
+./plot_gas.py
 echo ./plot_aero_kappa.py
+./plot_aero_kappa.py
 echo ./plot_aero_comp_bc.py
+./plot_aero_comp_bc.py
 echo 'View out/*.pdf'

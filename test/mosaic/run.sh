d7ca69cc001bc38c0ee39cd5edbd4346b69c0a96
Matthew West <mwest@illinois.edu>
2007-12-09 18:14:29 +0000

    moving mosaic testcase to subdir

--- test/mosaic/run.sh
+++ test/mosaic/run.sh
@@ -10,9 +10,9 @@ This tests the interface to the MOSAIC chemistry code.
 ENDINFO
 sleep 1
 
-echo ../src/partmc mosaic.spec
-../src/partmc mosaic.spec
+echo ../../src/partmc run_mc.spec
+../../src/partmc run_mc.spec
 
-echo ./mosaic_plot.py
-./mosaic_plot.py
+echo ./plot.py
+./plot.py
 echo Now view out/mosaic.pdf

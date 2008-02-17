c99b4a95ff824d9c56a9ea7613ebeab96e8587e3
Matthew West <mwest@illinois.edu>
2008-02-17 19:22:28 +0000

    pulled spilt run.sh/README files in test/ from 1.1.0 -r4343 by hand

--- test/golovin/run.sh
+++ test/golovin/run.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-cat <<ENDINFO
-
-Golovin Test-case
------------------
-
-This tests the Monte Carlo coagulation code for the Golovin kernel
-with an exponential initial condition for which an analytical solution
-is known. The Monte Carlo code is looped and average to reduce
-error. To improve the quality of the results either the number of
-loops or the number of particles can be increased.
-
-This test-case demonstrates generating exact solutions and using
-repeated loops of the Monte Carlo code to reduce the error, and serves
-as model verification against an analytical solution.
-
-ENDINFO
-#sleep 1
+cat README
+sleep 1
 
 echo ../../src/partmc run_mc.spec
 ../../src/partmc run_mc.spec

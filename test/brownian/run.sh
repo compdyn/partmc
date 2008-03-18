c0407f6f8dd3d0436a5af6fcffd8fb2ff20074d7
Nicole Riemer <nriemer@illinois.edu>
2008-03-18 16:45:16 +0000

    work on brownian test case

---
+++ test/brownian/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cat README
+sleep 1
+
+echo ../../src/partmc run_mc.spec
+../../src/partmc run_mc.spec
+echo ../../src/partmc run_sect.spec
+../../src/partmc run_sect.spec
+
+echo ./plot.py
+./plot.py
+echo "Now view out/brown_*.pdf"

6c0501bdd733c1234b96e4d7c053a41932a4b9c9
Nicole Riemer <nriemer@illinois.edu>
2008-05-14 18:29:28 +0000

    added fig_brownian in test/brown

---
+++ test/brownian/fig_brownian.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# Copyright (C) 2007, 2008 Matthew West
+# Licensed under the GNU General Public License version 2 or (at your
+# option) any later version. See the file COPYING for details.
+
+import os, sys
+import copy as module_copy
+from Scientific.IO.NetCDF import *
+from pyx import *
+sys.path.append("../../tool")
+from pmc_data_nc import *
+from pmc_pyx import *
+
+times_min = [0, 720, 1440]
+times_sec = [t * 60 for t in times_min]
+times_hour = [t / 60  for t in times_min]
+
+mc_data = pmc_var(NetCDFFile("out/try1/brown_mc_avg.nc"),
+		  "aero",
+		  [sum("aero_species")])
+sect_data = pmc_var(NetCDFFile("out/try1/brown_sect_0001.nc"),
+		    "aero",
+		    [sum("aero_species")])
+
+mc_data.scale_dim("radius", 2e6)
+sect_data.scale_dim("radius",2e6)
+
+g_num_lin = graph.graphxy(
+    width = 6,
+    x = graph.axis.log(min = 1e-2,
+		       max = 1e0,
+		       title = "diameter ($\mu$m)",
+		       painter = grid_painter),
+    y = graph.axis.linear(min = 0,
+			  max = 5e10,
+			  title = "number density (\#/m$^3$)",
+			  painter = grid_painter))
+
+g_vol_lin = graph.graphxy(
+    width = 6,
+    x = graph.axis.log(min = 1e-2,
+		       max = 1e0,
+		       title = "diameter ($\mu$m)",
+		       painter = grid_painter),
+    y = graph.axis.linear(min = 0,
+			  max = 4e-11,
+			  title = "volume density (m$^3$/m$^3$)",
+			  painter = grid_painter))
+
+for i in range(len(times_sec)):
+    data_slice = module_copy.deepcopy(mc_data)
+    data_slice.reduce([select("unit", "num_den"),
+		       select("time", times_sec[i])])
+    g_num_lin.plot(
+	graph.data.list(data_slice.data_center_list(strip_zero = True),
+			x = 1, y = 2,
+			title = "%g hours MC" % times_hour[i]),
+	styles = [graph.style.symbol(symbol = graph.style.symbol.circle,
+				     size = 0.05,
+				     symbolattrs = [color.grey.black])])
+    g_num_lin.text(1.5,2.9,"0 h",[text.halign.boxleft,text.valign.bottom,color.rgb(0,0,0)])
+    g_num_lin.text(2.5,1.1,"12 h",[text.halign.boxleft,text.valign.bottom,color.rgb(0,0,0)])
+    g_num_lin.text(3,0.1,"24 h",[text.halign.boxleft,text.valign.bottom,color.rgb(0,0,0)])
+
+    data_slice = module_copy.deepcopy(mc_data)
+    data_slice.reduce([select("unit", "vol_den"),
+		       select("time", times_sec[i])])
+    g_vol_lin.plot(
+	graph.data.list(data_slice.data_center_list(strip_zero = True),
+			x = 1, y = 2,
+			title = "%g hours MC" % times_hour[i]),
+	styles = [graph.style.symbol(symbol = graph.style.symbol.circle,
+				     size = 0.05,
+				     symbolattrs = [color.grey.black])])
+    g_vol_lin.text(2.3,1.1,"0 h",[text.halign.boxleft,text.valign.bottom,color.rgb(0,0,0)])
+    g_vol_lin.text(2.8,2.2,"12 h",[text.halign.boxleft,text.valign.bottom,color.rgb(0,0,0)])
+    g_vol_lin.text(3.3,3.3,"24 h",[text.halign.boxleft,text.valign.bottom,color.rgb(0,0,0)])
+#    g_vol_lin.stroke(path.line(2.2, 0.6, 2.95, 0.5))
+
+
+    data_slice = module_copy.deepcopy(sect_data)
+    data_slice.reduce([select("unit", "num_den"),
+		       select("time", times_sec[i])])
+
+    g_num_lin.plot(
+	graph.data.list(data_slice.data_center_list(strip_zero = True),
+			x = 1, y = 2,
+			title = "%g hours sect" % times_hour[i]),
+	styles = [graph.style.line(lineattrs = [color.grey.black])])
+    
+    data_slice = module_copy.deepcopy(sect_data)
+    data_slice.reduce([select("unit", "vol_den"),
+		       select("time", times_sec[i])])
+    g_vol_lin.plot(
+	graph.data.list(data_slice.data_center_list(strip_zero = True),
+			x = 1, y = 2,
+			title = "%g hours sect" % times_hour[i]),
+	styles = [graph.style.line(lineattrs = [color.grey.black])])
+
+    
+g_num_lin.writePDFfile("out/try1/brown_num_lin.pdf")
+g_vol_lin.writePDFfile("out/try1/brown_vol_lin.pdf")
+print "num_lin figure height = %.1f cm" % unit.tocm(g_num_lin.bbox().height())
+print "num_lin figure width = %.1f cm" % unit.tocm(g_num_lin.bbox().width())
+
+print "vol_lin figure height = %.1f cm" % unit.tocm(g_vol_lin.bbox().height())
+print "vol_lin figure width = %.1f cm" % unit.tocm(g_vol_lin.bbox().width())
+

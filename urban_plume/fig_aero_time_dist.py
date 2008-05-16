bb5a8bd54c631e3e00d382b48081d38ffb730554
Nicole Riemer <nriemer@illinois.edu>
2008-05-16 19:28:56 +0000

    changed fig_aero_time_dist.py

--- urban_plume/fig_aero_time_dist.py
+++ urban_plume/fig_aero_time_dist.py
@@ -11,63 +11,85 @@ sys.path.append("../tool")
 from pmc_data_nc import *
 from pmc_pyx import *
 
-aero_species = [["NO3"],
-                ["NH4"],
-                ["OC"],
-                ["SO4"],
-                ["BC"],
-                ["ARO1", "ARO2", "ALK1", "OLE1" ]]
-line_style_order = [4, 5, 0, 1, 2, 3]
-key_names = [ None, None, None, None, None, "SOA"]  # None means use default
+aero_species_1 = [["NO3"], ["NH4"], ["OC"]]
+aero_species_2 = [["SO4"], ["BC"], ["ARO1", "ARO2", "ALK1", "OLE1"]]
+
 
 data = pmc_var(NetCDFFile("out/urban_plume_with_coag_0001.nc"),
 	       "aero",
 	       [sum("dry_radius"),
 		select("unit", "mass_den")])
-#data.write_summary(sys.stdout)
 
 data.scale_dim("time", 1.0/60)
 data.scale(1e9)
 
-g = graph.graphxy(
-    width = 6.7,
-    x = graph.axis.linear(min = 0,
+c = canvas.canvas()
+
+g2 = c.insert(graph.graphxy(
+    width = 6.5,
+    x = graph.axis.linear(min = 0.,
                           max = 1440,
-                          title = "local standard time",
-#                          parter = graph.axis.parter.linear(tickdists = [6, 3]),
-#                          max = max(data.dim_by_name("time").grid_centers),
                           parter = graph.axis.parter.linear(tickdists
                                                             = [6 * 60, 3 * 60]),
                           texter = time_of_day(base_time = 6 * 60),
+                          title = "local standard time",
 			  painter = grid_painter),
-    y = graph.axis.linear(title = "mass density ($\mu$g/m$^3$)",
-			  painter = grid_painter),
-    key = graph.key.key(pos = "tc",
-                        vinside = 0,
-                        columns = 3,
-                        symbolwidth = 0.75 * unit.v_cm,
-                        keyattrs = [deco.stroked, deco.filled([color.gray.white])]))
-# print max(data.dim_by_name("time").grid_centers)
-for i in range(len(aero_species)):
+    y = graph.axis.linear(min = 0.,
+                          max = 10,
+                          title = "mass density ($\mu$g/m$^3$)",
+			  painter = grid_painter)))
+g1 = c.insert(graph.graphxy(
+    width = 6.5,
+    ypos = g2.height + 0.5,
+    x = graph.axis.linkedaxis(g2.axes["x"],
+                              painter = graph.axis.painter.linked(gridattrs = [style.linestyle.dotted])),
+    y = graph.axis.linear(min = 0.,
+                          max = 60,
+                          title = "mass density ($\mu$g/m$^3$)",
+			  painter = grid_painter)))
+
+for i in range(len(aero_species_1)):
     data_slice = module_copy.deepcopy(data)
-    data_slice.reduce([sum("aero_species", only = aero_species[i])])
-    if key_names[i] == None:
-        title_name =  tex_species(aero_species[i][0])
-    else:
-        title_name = key_names[i]
-        
-    g.plot(graph.data.points(data_slice.data_center_list(),
+    data_slice.reduce([sum("aero_species", only = aero_species_1[i])])
+    g1.plot(graph.data.points(data_slice.data_center_list(),
 			   x = 1, y = 2,
-			   title = title_name),
-	   styles = [graph.style.line(lineattrs = [line_style_list[line_style_order[i]],style.linewidth.Thick])])
+			   title = tex_species(aero_species_1[i])),
+	   styles = [graph.style.line(lineattrs = [line_style_list[i], style.linewidth.Thick])])
 
-#data.reduce([sum("aero_species")])
-#g.plot(graph.data.points(data.data_center_list(),
- #                      x = 1, y = 2,
- #                      title = "total"),
- #      styles = [graph.style.line(lineattrs
- #                                 = [color_list[len(aero_species)]])])
+for i in range(len(aero_species_2)):
+    data_slice = module_copy.deepcopy(data)
+    data_slice.reduce([sum("aero_species", only = aero_species_2[i])])
+    g2.plot(graph.data.points(data_slice.data_center_list(),
+			   x = 1, y = 2,
+			   title = tex_species(aero_species_2[i])),
+	   styles = [graph.style.line(lineattrs = [line_style_list[i], style.linewidth.Thick])])
 
-g.writePDFfile("figs/aero_time_dist.pdf")
-print "figure height = %.1f cm" % unit.tocm(g.bbox().height())
-print "figure width = %.1f cm" % unit.tocm(g.bbox().width())
+g1.text(g1.xpos + 1.8,
+        g1.ypos + 3.5,
+        r"$\rm NO_3$",
+        [text.halign.boxleft,text.valign.bottom,color.rgb(0,0,0)])
+g1.text(g1.xpos + 1.8,
+        g1.ypos + 1.3,
+        r"$\rm NH_4$",
+        [text.halign.boxleft,text.valign.bottom,color.rgb(0,0,0)])
+g1.text(g1.xpos + 2.6,
+        g1.ypos + 0.8,
+        r"OC",
+        [text.halign.boxleft,text.valign.bottom,color.rgb(0,0,0)])
+
+g2.text(g2.xpos + 1.6,
+        g2.ypos + 3.1,
+        r"$\rm SO_4$",
+        [text.halign.boxleft,text.valign.bottom,color.rgb(0,0,0)])
+g2.text(g2.xpos + 2.1,
+        g2.ypos + 2,
+        r"$\rm BC$",
+        [text.halign.boxleft,text.valign.bottom,color.rgb(0,0,0)])
+g2.text(g2.xpos + 2.4,
+        g2.ypos + 1.3,
+        r"$\rm SOA$",
+        [text.halign.boxleft,text.valign.bottom,color.rgb(0,0,0)])
+
+c.writePDFfile("figs/aero_time_dist.pdf")
+print "figure height = %.1f cm" % unit.tocm(c.bbox().height())
+print "figure width = %.1f cm" % unit.tocm(c.bbox().width())

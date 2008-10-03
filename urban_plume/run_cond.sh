33c202d2ba2bab31af4d2c189256eda4570e7322
Joseph Ching <ching1@illinois.edu>
2008-10-03 17:37:11 +0000

    run_cond.sh added

---
+++ urban_plume/run_cond.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cat <<ENDINFO
+
+Urban Plume Test-case
+---------------------
+
+This simulates an idealized urban plume with gas and aerosol
+chemistry.
+
+ENDINFO
+sleep 1
+
+echo ../src/partmc cond_test.spec
+../src/partmc cond_test.spec

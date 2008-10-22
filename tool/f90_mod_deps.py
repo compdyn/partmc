a746345759a255563fbf96167bc91a3837e0d6a1
Matthew West <mwest@illinois.edu>
2008-10-22 18:17:27 +0000

    added graphviz/dot module dependency diagram generation

--- tool/f90_mod_deps.py
+++ tool/f90_mod_deps.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright (C) 2006-2007 Matthew West
+# Copyright (C) 2006-2008 Matthew West
 # Licensed under the GNU General Public License version 2 or (at your
 # option) any later version. See the file COPYING for details.
 
@@ -36,6 +36,15 @@ module and use statements in them. Options are:
                  produced from each module matched by the mod-re
                  regular expression.
 
+  -g, --graph    Produce output in .dot format suitable for use with
+                 graphviz.
+
+  -i, --indirect
+                 Allow indirect references (only for graphing), so if
+                 A depends on B and C, and B depends on C, then the
+                 direct dependency of A on C will dropped as there is
+                 an indirect dependency via B.
+
   -v, --verbose  Turn on verbose debugging output.
 
 For a discussion of managing Fortran 90 dependencies see:
@@ -54,6 +63,8 @@ class Opts:
     dep_template = "\\1.mod"
     mod_re = "(.*)"
     mod_template = "\\1.mod"
+    output_format = "make"
+    indirect = False
     verbose = False
 
 def get_deps_and_mods(filename, opts):
@@ -115,19 +126,82 @@ def get_deps_and_mods(filename, opts):
     f.close()
     return (deps, mods)
 
-def write_deps(outf, filename, deps, mods):
+def write_deps_make(outf, filename, deps, mods):
     filebase, fileext = os.path.splitext(filename)
     outf.write("%s.o: %s\n" % (filebase, " ".join(deps)))
     for mod in mods:
 	outf.write("%s: %s\n"
 		   % (mod, " ".join(deps)))
 
+def write_deps_graphviz(outf, filename, all_deps, has_mods):
+    outf.write("    node [shape = box, peripheries = 2];")
+    for mod in all_deps.keys():
+        if not has_mods[mod]:
+            outf.write("    %s\n" % mod)
+    outf.write("    node [shape = box, peripheries = 1];")
+    for mod in all_deps.keys():
+        for dep in all_deps[mod]:
+            outf.write("    %s -> %s\n" % (mod, dep))
+
+def propagrate_deps(full_deps, opts):
+    if opts.verbose:
+        sys.stderr.write("propagating full dependencies...\n")
+    changes_made = False
+    if opts.verbose:
+        sys.stderr.write("for modules: %s\n" % " ".join(full_deps.keys()))
+    for mod in full_deps.keys():
+        new_deps = set()
+        for dep in full_deps[mod]:
+            new_deps |= full_deps[dep]
+        if opts.verbose:
+            sys.stderr.write("current dependencies of %s: %s\n"
+                             % (mod, " ".join(full_deps[mod])))
+            sys.stderr.write("new dependencies of %s: %s\n"
+                             % (mod, " ".join(new_deps)))
+        if new_deps > full_deps[mod]:
+            if opts.verbose:
+                sys.stderr.write("adding dependencies to %s: %s\n"
+                                 % (mod, " ".join(new_deps - full_deps[mod])))
+            changes_made = True
+            full_deps[mod] = new_deps
+    if opts.verbose:
+        if changes_made:
+            sys.stderr.write("dependencies were updated\n")
+        else:            
+            sys.stderr.write("no updates to dependencies\n")
+    return changes_made
+
+def remove_with_indirect(all_deps, opts):
+    full_deps = {}
+    for mod in all_deps.keys():
+        full_deps[mod] = set(all_deps[mod])
+    while propagrate_deps(full_deps, opts):
+        pass
+    if opts.verbose:
+        sys.stderr.write("computing minimal dependencies...\n")
+    for mod in all_deps.keys():
+        if opts.verbose:
+            sys.stderr.write("computing minimal dependencies for %s...\n" % mod)
+        deps = set(all_deps[mod])
+        if opts.verbose:
+            sys.stderr.write("direct dependencies: %s\n" % " ".join(deps))
+        for dep in all_deps[mod]:
+            if opts.verbose:
+                sys.stderr.write("removing via %s: %s\n"
+                                 % (dep, " ".join(full_deps[dep])))
+            deps -= full_deps[dep]
+        all_deps[mod] = [x for x in deps]
+        if opts.verbose:
+            sys.stderr.write("minimal dependencies of %s: %s\n"
+                             % (mod, " ".join(all_deps[mod])))
+
 def process_args():
     try:
-	opts, args = getopt.getopt(sys.argv[1:], "ho:d:D:m:M:v",
+	opts, args = getopt.getopt(sys.argv[1:], "ho:d:D:m:M:vgi",
 				   ["help", "output=", "dep-re=",
 				    "dep-template=", "mod-re=",
-				    "mod-template=", "verbose"])
+				    "mod-template=", "graph",
+                                    "indirect", "verbose"])
     except getopt.GetoptError:
 	print "ERROR: invalid commandline options"
 	usage()
@@ -147,6 +221,10 @@ def process_args():
 	    myopts.mod_re = a
 	if o in ("-M", "--mod-template"):
 	    myopts.mod_template = a
+	if o in ("-g", "--graph"):
+	    myopts.output_format = "graphviz"
+	if o in ("-i", "--indirect"):
+	    myopts.indirect = True
 	if o in ("-v", "--verbose"):
 	    myopts.verbose = True
 	    sys.stderr.write("Verbose output on\n")
@@ -159,6 +237,9 @@ def process_args():
 	sys.stderr.write("dep-template = %s\n" % myopts.dep_template)
 	sys.stderr.write("mod-re = %s\n" % myopts.mod_re)
 	sys.stderr.write("mod-template = %s\n" % myopts.mod_template)
+	sys.stderr.write("output-format = %s\n" % myopts.output_format)
+	sys.stderr.write("indirect = %s\n" % myopts.indirect)
+	sys.stderr.write("verbose = %s\n" % myopts.verbose)
     return (myopts, args)
 
 def main():
@@ -171,14 +252,41 @@ def main():
 	outf = sys.stdout
 	if opts.verbose:
 	    sys.stderr.write("Output to STDOUT\n")
-    outf.write("# DO NOT EDIT --- auto-generated file\n")
-    for filename in filenames:
-	(deps, mods) = get_deps_and_mods(filename, opts)
-	if opts.verbose:
-	    sys.stderr.write("deps: %s\n" % " ".join(deps))
-	    sys.stderr.write("mods: %s\n" % " ".join(mods))
-	if deps:
-	    write_deps(outf, filename, deps, mods)
+    if opts.output_format == "make":
+        outf.write("# DO NOT EDIT --- auto-generated file\n")
+        for filename in filenames:
+            (deps, mods) = get_deps_and_mods(filename, opts)
+            if opts.verbose:
+                sys.stderr.write("deps: %s\n" % " ".join(deps))
+                sys.stderr.write("mods: %s\n" % " ".join(mods))
+            if deps:
+                write_deps_make(outf, filename, deps, mods)
+    elif opts.output_format == "graphviz":
+        all_deps = {}
+        has_mods = {}
+        for filename in filenames:
+            (deps, mods) = get_deps_and_mods(filename, opts)
+            if opts.verbose:
+                sys.stderr.write("processing %s\n" % filename)
+                sys.stderr.write("deps: %s\n" % " ".join(deps))
+                sys.stderr.write("mods: %s\n" % " ".join(mods))
+            dir, file = os.path.split(filename)
+            filebase, fileext = os.path.splitext(file)
+            if opts.verbose:
+                sys.stderr.write("adding deps to hash: ")
+                sys.stderr.write("%s: %s\n" % (mod, " ".join(deps)))
+            all_deps[filebase] = deps
+            if mods:
+                has_mods[filebase] = True
+            else:
+                has_mods[filebase] = False
+        if opts.indirect:
+            if opts.verbose:
+                sys.stderr.write("using indirect dependencies\n")
+            remove_with_indirect(all_deps, opts)
+        write_deps_graphviz(outf, filename, all_deps, has_mods)
+    else:
+        raise Exception("unknown output_format: %s" % opts.output_format)
     outf.close()
 
 if __name__ == "__main__":

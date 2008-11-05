577d5c47ebd8ca3187f01d60bc6a4ab03cb163ba
Nicole Riemer <nriemer@illinois.edu>
2008-11-05 16:44:36 +0000

    changed process_aging.py

--- urban_plume/process_aging.py
+++ urban_plume/process_aging.py
@@ -12,16 +12,20 @@ from pmc_data_nc import *
 from pmc_pyx import *
 from fig_helper import *
 
-time_filename_list_wc = get_time_filename_list(netcdf_dir_wc, netcdf_pattern_wc)
-env_state = read_any(env_state_t, netcdf_dir_wc, netcdf_pattern_wc)
+time_filename_list_nc = get_time_filename_list(netcdf_dir_nc, netcdf_pattern_nc)
+env_state = read_any(env_state_t, netcdf_dir_nc, netcdf_pattern_nc)
 start_time_of_day_min = env_state.start_time_of_day / 60
-max_time_min = max([time for [time, filename, key] in time_filename_list_wc]) / 60
+max_time_min = max([time for [time, filename, key] in time_filename_list_nc]) / 60
 
-outf_a = open("aging_a.dat", "w")
-outf_p = open("aging_p.dat", "w")
+outf_a = open("aging_a_nc", "w")
+outf_p = open("aging_p_nc", "w")
+outf_ea = open("aging_ea_nc", "w")
+outf_ep = open("aging_ep_nc", "w")
 
-for [time, filename, key] in time_filename_list_wc:
-    print filename
+old_id = set()
+
+for [time, filename, key] in time_filename_list_nc:
+    print time, filename
     ncf = NetCDFFile(filename)
     particles = aero_particle_array_t(ncf)
     env_state = env_state_t(ncf)
@@ -33,17 +37,35 @@ for [time, filename, key] in time_filename_list_wc:
     outf_p.write("%f " % time)
     for ss_activ in [0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009, 0.01]:
         aged_num_den = 0.0
+        fresh_num_den = 0.0
+        aged_emissions = 0.0
+        fresh_emissions = 0.0
+        
         for i in range(num_den.size):
+            # i is particle index
+            if particles.id[i] not in old_id:    
+                if critical_ss[i] < ss_activ:
+                    aged_emissions += num_den[i]
+                else:
+                    fresh_emissions += num_den[i]
             if critical_ss[i] < ss_activ:
                 aged_num_den += num_den[i]
-        fresh_num_den = total_num_den - aged_num_den
-        print time / 60., ss_activ, aged_num_den, fresh_num_den, total_num_den
+            else:
+                fresh_num_den += num_den[i]
+        print time / 60., ss_activ, aged_emissions, fresh_emissions, aged_num_den, fresh_num_den, total_num_den
         outf_a.write("%e " % aged_num_den)
         outf_p.write("%e " % fresh_num_den)
+        outf_ea.write("%e " % aged_emissions)
+        outf_ep.write("%e " % fresh_emissions)
     outf_a.write("\n")
     outf_p.write("\n")
-
+    outf_ea.write("\n")
+    outf_ep.write("\n")
+    old_id = set()
+    for i in range(num_den.size):
+        old_id.add(particles.id[i]) 
 outf_a.close()
 outf_p.close()
-
+outf_ea.close()
+outf_ep.close()
 

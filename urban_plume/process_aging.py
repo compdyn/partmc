d57accb27d6fce2a4ca26ca13d184d32ebff9efe
Nicole Riemer <nriemer@illinois.edu>
2008-12-01 23:14:51 +0000

    deleted commented lines in temp.dat

--- urban_plume/process_aging.py
+++ urban_plume/process_aging.py
@@ -12,15 +12,16 @@ from pmc_data_nc import *
 from pmc_pyx import *
 from fig_helper import *
 
-time_filename_list_nc = get_time_filename_list(netcdf_dir_nc, netcdf_pattern_nc)
-env_state = read_any(env_state_t, netcdf_dir_nc, netcdf_pattern_nc)
+time_filename_list_nc = get_time_filename_list(netcdf_dir_wc, netcdf_pattern_wc)
+env_state = read_any(env_state_t, netcdf_dir_wc, netcdf_pattern_wc)
 start_time_of_day_min = env_state.start_time_of_day / 60
 max_time_min = max([time for [time, filename, key] in time_filename_list_nc]) / 60
 
-outf_a = open("aging_a_nc", "w")
-outf_p = open("aging_p_nc", "w")
-outf_ea = open("aging_ea_nc", "w")
-outf_ep = open("aging_ep_nc", "w")
+outf_a = open("aging_a_wc", "w")
+outf_f = open("aging_f_wc", "w")
+outf_ea = open("aging_ea_wc", "w")
+outf_ef = open("aging_ef_wc", "w")
+outf_h = open("aging_h_wc", "w")
 
 old_id = set()
 
@@ -33,10 +34,11 @@ for [time, filename, key] in time_filename_list_nc:
     num_den = 1.0 / particles.comp_vol
     critical_ss = particles.kappa_rh(env_state) - 1.0
     total_num_den = num_den.sum()
+    outf_h.write("%f %e\n" % (time, env_state.height))
     outf_a.write("%f " % time)
-    outf_p.write("%f " % time)
+    outf_f.write("%f " % time)
     outf_ea.write("%f " % time)
-    outf_ep.write("%f " % time)
+    outf_ef.write("%f " % time)
 
     for ss_activ in [0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009, 0.01]:
         aged_num_den = 0.0
@@ -57,18 +59,19 @@ for [time, filename, key] in time_filename_list_nc:
                 fresh_num_den += num_den[i]
         print time / 60., ss_activ, aged_emissions, fresh_emissions, aged_num_den, fresh_num_den, total_num_den
         outf_a.write("%e " % aged_num_den)
-        outf_p.write("%e " % fresh_num_den)
+        outf_f.write("%e " % fresh_num_den)
         outf_ea.write("%e " % aged_emissions)
-        outf_ep.write("%e " % fresh_emissions)
+        outf_ef.write("%e " % fresh_emissions)
     outf_a.write("\n")
-    outf_p.write("\n")
+    outf_f.write("\n")
     outf_ea.write("\n")
-    outf_ep.write("\n")
+    outf_ef.write("\n")
     old_id = set()
     for i in range(num_den.size):
         old_id.add(particles.id[i]) 
 outf_a.close()
-outf_p.close()
+outf_f.close()
 outf_ea.close()
-outf_ep.close()
+outf_ef.close()
+outf_h.close()
 

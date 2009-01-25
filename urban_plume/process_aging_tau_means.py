bdf752b91e0e956875f5118ca765996dfe7dff16
Matthew West <mwest@illinois.edu>
2009-01-25 21:27:50 +0000

    added type to loadtxt, unknown if it makes a difference

--- urban_plume/process_aging_tau_means.py
+++ urban_plume/process_aging_tau_means.py
@@ -7,8 +7,32 @@ import os, sys, pyx
 from numpy import *
 sys.path.append("../tool")
 from pmc_pyx import *
-sys.path.append(".")
-from process_aging_config import *
+
+data_prefix = "aging_data/10"
+
+data = loadtxt("%s/aging_tau_means.txt" % data_prefix, float)
+
+ss_active = data[:,1]
+
+tau_day_wc_num = data[:,2]
+tau_day_wc_cond_num = data[:,3]
+tau_night_wc_num = data[:,4]
+tau_night_wc_cond_num = data[:,5]
+
+tau_day_wc_mass = data[:,6]
+tau_day_wc_cond_mass = data[:,7]
+tau_night_wc_mass = data[:,8]
+tau_night_wc_cond_mass = data[:,9]
+
+tau_day_nc_num = data[:,10]
+tau_day_nc_cond_num = data[:,11]
+tau_night_nc_num = data[:,12]
+tau_night_nc_cond_num = data[:,13]
+
+tau_day_nc_mass = data[:,14]
+tau_day_nc_cond_mass = data[:,15]
+tau_night_nc_mass = data[:,16]
+tau_night_nc_cond_mass = data[:,17]
 
 g = pyx.graph.graphxy(
     width = 20,
@@ -27,22 +51,22 @@ solid = pyx.style.linestyle.solid
 dashed = pyx.style.linestyle(pyx.style.linecap.round, pyx.style.dash([2, 2]))
 dashdot = pyx.style.linestyle(pyx.style.linecap.round, pyx.style.dash([0, 2, 2, 2]))
 
-data = [
+plotting_data = [
     [tau_day_wc_mass, "tau day wc mass", red, dashdot],
     [tau_day_wc_cond_mass, "tau day wc cond mass", red, solid],
-    [tau_day_nc_mass, "tau day nc mass", red, dashed],
+#    [tau_day_nc_mass, "tau day nc mass", red, dashed],
     [tau_day_wc_num, "tau day wc num", magenta, dashdot],
     [tau_day_wc_cond_num, "tau day wc cond num", magenta, solid],
-    [tau_day_nc_num, "tau day nc num", magenta, dashed],
+#    [tau_day_nc_num, "tau day nc num", magenta, dashed],
     [tau_night_wc_mass, "tau night wc mass", blue, dashdot],
     [tau_night_wc_cond_mass, "tau night wc cond mass", blue, solid],
-    [tau_night_nc_mass, "tau night nc mass", blue, dashed],
+#    [tau_night_nc_mass, "tau night nc mass", blue, dashed],
     [tau_night_wc_num, "tau night wc num", cyan, dashdot],
     [tau_night_wc_cond_num, "tau night wc cond num", cyan, solid],
-    [tau_night_nc_num, "tau night nc num", cyan, dashed],
+#    [tau_night_nc_num, "tau night nc num", cyan, dashed],
     ]
 
-for [tau_data, label, color, pattern] in data:
+for [tau_data, label, color, pattern] in plotting_data:
     tau_data_hour = [t / 3600 for t in tau_data]
     g.plot(
         pyx.graph.data.points(zip(ss_active, tau_data_hour), x = 1, y = 2,
@@ -50,4 +74,3 @@ for [tau_data, label, color, pattern] in data:
         styles = [pyx.graph.style.line(lineattrs = [color, pattern])])
 
 g.writePDFfile("%s/aging_tau.pdf" % data_prefix)
-

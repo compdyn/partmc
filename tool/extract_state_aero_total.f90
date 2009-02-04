77f90de6da244b84fd9079962d91c4cde01b77dc
Matthew West <mwest@illinois.edu>
2009-02-04 15:34:48 +0000

    added extract_state_aero_total

--- tool/extract_state_aero_total.f90
+++ tool/extract_state_aero_total.f90
@@ -2,10 +2,10 @@
 ! Licensed under the GNU General Public License version 2 or (at your
 ! option) any later version. See the file COPYING for details.
 !
-! Read NetCDF state files and write out the aerosol concentrations in
-! text format.
+! Read NetCDF state files and write out the total aerosol number and
+! mass concentrations in text format.
 
-program extract_state_aero_size_num
+program extract_state_aero_total
 
   use netcdf
 
@@ -15,34 +15,26 @@ program extract_state_aero_size_num
   integer :: ncid
   integer :: dimid_aero_species, dimid_aero_particle
   integer :: varid_time, varid_aero_species
-  integer :: varid_aero_comp_mass, varid_aero_density
+  integer :: varid_aero_comp_mass
   integer :: varid_aero_comp_vol
   integer :: n_aero_species, n_aero_particle
   character(len=1000) :: tmp_str, aero_species_names
   real*8 :: time
   real*8, allocatable :: aero_comp_mass(:,:)
-  real*8, allocatable :: aero_density(:)
   real*8, allocatable :: aero_comp_vol(:)
-  real*8, allocatable :: aero_dist(:)
   integer :: xtype, ndims, nAtts
   integer, dimension(nf90_max_var_dims) :: dimids
-  integer :: ios, i_time, i_spec, i_part, status
+  integer :: ios, i_time, i_part, status
   integer :: n_bin, i_bin
-  real*8 :: r_min, r_max, radius, volume
+  real*8 :: num_conc, mass_conc
 
   ! process commandline arguments
-  if (iargc() .ne. 5) then
-     write(6,*) 'Usage: extract_state_aero_size_num <r_min> <r_max> <n_bin> <netcdf_state_prefix> <output_filename>'
+  if (iargc() .ne. 2) then
+     write(6,*) 'Usage: extract_state_total <netcdf_state_prefix> <output_filename>'
      call exit(2)
   endif
-  call getarg(1, tmp_str)
-  r_min = string_to_real(tmp_str)
-  call getarg(2, tmp_str)
-  r_max = string_to_real(tmp_str)
-  call getarg(3, tmp_str)
-  n_bin = string_to_integer(tmp_str)
-  call getarg(4, in_prefix)
-  call getarg(5, out_filename)
+  call getarg(1, in_prefix)
+  call getarg(2, out_filename)
 
   ! open output file
   open(unit=out_unit, file=out_filename, iostat=ios)
@@ -51,22 +43,14 @@ program extract_state_aero_size_num
           trim(out_filename), ' for writing: ', ios
      call exit(1)
   end if
-  write(out_unit, '(e30.15e3)', advance='no') 0d0
-  do i_bin = 1,n_bin
-     radius = exp(dble(i_bin - 1) / dble(n_bin - 1) &
-          * (log(r_max) - log(r_min)) + log(r_min))
-     write(out_unit, '(e30.15e3)', advance='no') radius
-  end do
-  write(out_unit, '(a)') ''
 
   ! write information
   write(*,*) "Output file array A has:"
-  write(*,*) "  A(1, j+1) = radius(j) (m)"
-  write(*,*) "  A(i+1, 1) = time(i) (s)"
-  write(*,*) "  A(i+1, j+1) = number concentration at time(i) and radius(j) (#/m^3)"
+  write(*,*) "  A(i, 1) = time(i) (s)"
+  write(*,*) "  A(i, 2) = total number concentration at time(i) (#/m^3)"
+  write(*,*) "  A(i, 3) = total mass concentration at time(i) (kg/m^3)"
 
   ! process NetCDF files
-  allocate(aero_dist(n_bin))
   i_time = 0
   do while (.true.)
      i_time = i_time + 1
@@ -112,20 +96,6 @@ program extract_state_aero_size_num
      call nc_check(nf90_get_var(ncid, varid_aero_comp_mass, &
           aero_comp_mass))
      
-     ! read aero_density
-     call nc_check(nf90_inq_varid(ncid, "aero_density", &
-          varid_aero_density))
-     call nc_check(nf90_Inquire_Variable(ncid, varid_aero_density, &
-          tmp_str, xtype, ndims, dimids, nAtts))
-     if ((ndims /= 1) &
-          .or. (dimids(1) /= dimid_aero_species)) then
-        write(*,*) "ERROR: unexpected aero_density dimids"
-        call exit(1)
-     end if
-     allocate(aero_density(n_aero_species))
-     call nc_check(nf90_get_var(ncid, varid_aero_density, &
-          aero_density))
-     
      ! read aero_comp_vol
      call nc_check(nf90_inq_varid(ncid, "aero_comp_vol", &
           varid_aero_comp_vol))
@@ -142,32 +112,23 @@ program extract_state_aero_size_num
      
      call nc_check(nf90_close(ncid))
 
-     ! compute distribution
-     aero_dist = 0d0
+     ! compute number and mass concentrations
+     num_conc = 0d0
+     mass_conc = 0d0
      do i_part = 1,n_aero_particle
-        volume = sum(aero_comp_mass(i_part,:) / aero_density)
-        radius = (volume / (4d0 / 3d0 &
-             * 3.14159265358979323846d0))**(1d0/3d0)
-        i_bin = ceiling((log(radius) - log(r_min)) &
-             / (log(r_max) - log(r_min)) * dble(n_bin - 1) + 0.5d0)
-        aero_dist(i_bin) = aero_dist(i_bin) + 1d0 / aero_comp_vol(i_part)
+        num_conc = num_conc + 1d0 / aero_comp_vol(i_part)
+        mass_conc = mass_conc + sum(aero_comp_mass(i_part,:)) &
+             / aero_comp_vol(i_part)
      end do
 
      ! output data
-     write(out_unit, '(e30.15e3)', advance='no') time
-     do i_bin = 1,n_bin
-        write(out_unit, '(e30.15e3)', advance='no') &
-             aero_dist(i_bin)
-     end do
-     write(out_unit, '(a)') ''
+     write(out_unit, '(3e30.15e3)') time, num_conc, mass_conc
 
      deallocate(aero_comp_mass)
-     deallocate(aero_density)
      deallocate(aero_comp_vol)
   end do
 
   close(out_unit)
-  deallocate(aero_dist)
 
 contains
 
@@ -188,46 +149,4 @@ contains
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-  !> Convert a string to a real.
-  real*8 function string_to_real(string)
-
-    !> String to convert.
-    character(len=*), intent(in) :: string
-    
-    real*8 :: val
-    integer :: ios
-
-    read(string, '(e40.0)', iostat=ios) val
-    if (ios /= 0) then
-       write(0,'(a,a,a,i3)') 'Error converting ', trim(string), &
-            ' to real: IOSTAT = ', ios
-       call exit(2)
-    end if
-    string_to_real = val
-
-  end function string_to_real
-
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-  !> Convert a string to an integer.
-  integer function string_to_integer(string)
-
-    !> String to convert.
-    character(len=*), intent(in) :: string
-    
-    integer :: val
-    integer :: ios
-
-    read(string, '(i20)', iostat=ios) val
-    if (ios /= 0) then
-       write(0,'(a,a,a,i3)') 'Error converting ', trim(string), &
-            ' to integer: IOSTAT = ', ios
-       call exit(1)
-    end if
-    string_to_integer = val
-
-  end function string_to_integer
-
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-end program extract_state_aero_size_num
+end program extract_state_aero_total

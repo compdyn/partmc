763776cf140ff5fc3fcd2fc0d736b05d28165ef8
Matthew West <mwest@illinois.edu>
2009-02-04 20:17:22 +0000

    split test/golovin/run_test_1.sh into two parts, the second of which depends on the first

--- test/golovin/run_test_1.sh
+++ test/golovin/run_test_1.sh
@@ -6,13 +6,8 @@ cd ${0%/*}
 ../../partmc run_mc.spec
 ../../partmc run_exact.spec
 
-../../extract_summary_aero num out/golovin_mc_0001.nc out/golovin_mc_num.txt
-../../extract_summary_aero num out/golovin_exact_0001.nc out/golovin_exact_num.txt
-../../extract_summary_aero mass out/golovin_mc_0001.nc out/golovin_mc_mass.txt
-../../extract_summary_aero mass out/golovin_exact_0001.nc out/golovin_exact_mass.txt
+../../extract_summary_aero_size_num out/golovin_mc_0001.nc out/golovin_mc_num.txt
+../../extract_summary_aero_size_num out/golovin_exact_0001.nc out/golovin_exact_num.txt
 
 ../../numeric_diff out/golovin_mc_num.txt out/golovin_exact_num.txt 0 2e-2
-num_result=$?
-../../numeric_diff out/golovin_mc_mass.txt out/golovin_exact_mass.txt 0 2e-2
-mass_result=$?
-exit $(( num_result || mass_result ))
+exit $?

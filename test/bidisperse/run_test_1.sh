c3af72991b22f9d11f957df8f1b4e5af5a5fe55a
Matthew West <mwest@illinois.edu>
2009-06-07 21:30:47 +0000

    test/bidiperse echos commands before executing

--- test/bidisperse/run_test_1.sh
+++ test/bidisperse/run_test_1.sh
@@ -16,7 +16,9 @@ try_number=0
 while (( $mc_run_not_ok )) ; do
     try_number=$(( $try_number + 1 ))
     echo Try number $try_number
+    echo "../../partmc run_mc.spec"
     ../../partmc run_mc.spec
+    echo "../../test_bidisperse_extract"
     ../../test_bidisperse_extract
     mc_run_not_ok=$?
     if (( $mc_run_not_ok )) ; then
@@ -28,10 +30,13 @@ while (( $mc_run_not_ok )) ; do
     fi
 done
 
+echo "../../test_bidisperse_ode"
 ../../test_bidisperse_ode
 
 # extract size distributions for plotting
+echo "../../extract_state_aero_size_num 1e-8 1e0 255 out/bidisperse_mc_0001_ out/bidisperse_mc_aero_size_num.txt"
 ../../extract_state_aero_size_num 1e-8 1e0 255 out/bidisperse_mc_0001_ out/bidisperse_mc_aero_size_num.txt
 
+echo "../../numeric_diff out/bidisperse_mc_data.txt out/bidisperse_ode_data.txt 0 1e-5 0 0 1 1"
 ../../numeric_diff out/bidisperse_mc_data.txt out/bidisperse_ode_data.txt 0 1e-5 0 0 1 1
 exit $?

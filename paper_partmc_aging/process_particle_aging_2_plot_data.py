7b2057bdc1385e29b0ae7c1db02742ad8129aac9
Matthew West <mwest@illinois.edu>
2009-08-07 09:38:26 +0000

    aging calculations for plot

--- paper_partmc_aging/process_particle_aging_2_plot_data.py
+++ paper_partmc_aging/process_particle_aging_2_plot_data.py
@@ -52,69 +52,24 @@ for coag in [True]:
 
     num_den_array = numpy.zeros([diameter_axis.n_bin, aging_time_axis.n_bin])
 
+    time_aging = zeros_like(diameter)
+    time_aging[:] = -1.0
     for id in range(size(time_emitted_array)):
         if time_aging_array[id] >= 0.0:
             i = particle_index_by_id[id]
-            if bc_mass[i] > 0.0:
-                
+            time_aging[i] = time_aging_array[id]
 
+    diameter_bin = diameter_axis.find(diameter)
+    time_aging_bin = time_aging_axis.find(time_aging)
 
+    for i in range(size(diameter)):
+        if bc_mass[i] > 0.0:
+            num_den_array[diameter_bin[i], time_aging_bin[i]] += 1.0
 
-    time_emitted = {}
-    time_entered_bins = [{} for i in range(n_level_bin + 2)]
-
-    first_time = True
-    for [time, filename, key] in time_filename_list:
-        #DEBUG
-        #if time > 121:
-        #    sys.exit(0)
-        #DEBUG
-        print time, filename
-        ncf = NetCDFFile(filename)
-        particles = aero_particle_array_t(ncf)
-        particles.id = [int(i) for i in particles.id]
-        env_state = env_state_t(ncf)
-        ncf.close()
-        num_den = 1.0 / array(particles.comp_vol)
-        total_num_den = num_den.sum()
-        soot_mass = particles.mass(include = ["BC"])
-        critical_ss = particles.kappa_rh(env_state, const) - 1.0
-        ss_bin = ss_active_axis.find_clipped_outer(critical_ss)
-        id_set = set([particles.id[i] for i in range(particles.n_particles)])
-        particle_index_by_id = dict([[particles.id[i], i] for i in range(particles.n_particles)])
-
-        for i in range(particles.n_particles):
-            id = particles.id[i]
-            if id not in time_emitted:
-                time_emitted[id] = time
-            bin = ss_bin[i]
-            if id not in time_entered_bins[bin]:
-                time_entered_bins[bin][id] = time
-
-    max_id = max(time_emitted.keys())
+    value = num_den_array / num_den_array.sum() \
+            / x_axis.grid_size(0) / (y_axis.grid_size(0) / 100.0)
 
     filename = os.path.join(aging_data_dir,
-                            "particle_aging_%s_%%s.txt" % coag_suffix)
+                            "particle_aging_%s_plot_data_%08d.txt" % (coag_suffix, bin))
 
-    time_emitted_array = zeros(max_id)
-    time_emitted_array = -1.0
-    for (id, time) in time_emitted.iteritems():
-        time_emitted_array[id] = time
-    savetxt(filename % "time_emitted", time_emitted_array)
-
-    for bin in range(n_level_bin + 2):
-        print "bin", bin
-        filename_bin = filename % ("%08d_%%s" % bin)
-
-        time_entered_bins_array = zeros(max_id)
-        time_aging_array = zeros(max_id)
-
-        time_entered_bins_array = -1.0
-        time_aging_array = -1.0
-
-        for (id, time) in time_entered_bins[bin]:
-            time_entered_bins_array[id] = time
-            if time_emitted_array[id] < 0.0:
-                raise Exception("ID %d entered bin %d at time %f but was never emitted" % id, bin, time)
-            time_aging_array[id] = time - time_emitted_array[id]
-
+    savetxt(filename, value)

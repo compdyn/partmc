c8ab1f51e810337511dcb9179b060969f0a790c9
Nicole Riemer <nriemer@illinois.edu>
2009-12-07 04:34:52 +0000

    deleted start directory

---
+++ new_cond/make_specs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+for hour in range(1, 49):
+    print "hour = ", hour
+    filename_in = "cond_template.spec"
+    for run in ["ref", "comp", "size", "both"]: 
+        filename_out = "spec/cond_%02d_%s.spec" % (hour, run)
+        print "filename_out ", filename_out
+        f_in = open(filename_in, 'r')
+        f_out = open(filename_out, 'w')
+
+        for line in f_in:
+            line = line.replace('%%OUTPUT_PREFIX%%', 'out/cond_%02d_%s' % (hour, run))
+            if run == "ref":
+                line = line.replace('%%RESTART_FILE%%',  'start/urban_plume_wc_0001_000000%02d.nc' % hour )
+            else:
+                line = line.replace('%%RESTART_FILE%%',  'start/urban_plume_%s_wc_0001_000000%02d.nc' % (run, hour) )
+            line = line.replace('%%TEMP_PROFILE%%',  'temp/temp_%02d.dat' % hour)
+            f_out.write(line)
+
+        f_in.close()
+        f_out.close()
+
+    filename_out_temp = "temp/temp_%02d.dat" % hour
+    print "filename_out_temp", filename_out_temp
+    f_out = open(filename_out_temp, 'w')
+
+    f_out.write("# time (s)\n")
+    f_out.write("# temp (K)\n")
+    f_out.write("time  %.2f %.2f\n" % (hour * 3600.0, hour * 3600.0 + 1200))
+    f_out.write("temp  290   280\n")   
+    f_out.close()
+

b06274b2577d45879c854b45b149036060f00189
Nicole Riemer <nriemer@illinois.edu>
2010-01-27 01:05:44 +0000

    added banana-species-plots

---
+++ figures/8_nucleate/fig_banana_species.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import Scientific.IO.NetCDF
+import sys
+import numpy as np
+import matplotlib
+matplotlib.use("PDF")
+import matplotlib.pyplot as plt
+sys.path.append("../../tool")
+import pmc_data_nc
+
+dist_array = np.loadtxt("data/banana_so4_dist.txt")
+times = np.loadtxt("data/banana_times.txt")
+diam = np.loadtxt("data/banana_diam.txt")
+
+mask = np.ma.make_mask(dist_array <= 0.0)
+dist_array = np.ma.array(dist_array, mask = mask)
+times = times / 3600.
+
+plt.clf()
+plt.pcolor(times, diam, dist_array.transpose(),norm = matplotlib.colors.LogNorm(), linewidths = 0.1)
+a = plt.gca()
+a.set_xscale("linear")
+a.set_yscale("log")
+plt.axis([times.min(), times.max(), diam.min(), diam.max()])
+plt.xlabel("time (h)")
+plt.ylabel("dry diameter (m)")
+cbar = plt.colorbar()
+cbar.set_label("mass density (kg m^{-3})")
+fig = plt.gcf()
+fig.savefig("figs/banana_so4_wc.pdf")
+
+
+
+
---
+++ figures/8_nucleate/process_banana_species.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import Scientific.IO.NetCDF
+import sys
+import numpy as np
+import matplotlib
+matplotlib.use("PDF")
+import matplotlib.pyplot as plt
+sys.path.append("../../tool")
+import pmc_data_nc
+const = pmc_data_nc.load_constants("../../src/constants.f90")
+
+netcdf_dir = "/home/mwest/svn/partmc/trunk/urban_plume_nucleate/out/"
+netcdf_pattern = "urban_plume_wc_0001_(.*).nc"
+time_filename_list = pmc_data_nc.get_time_filename_list(netcdf_dir, netcdf_pattern)
+
+dist_array = np.zeros([len(time_filename_list),100])
+times = np.zeros([len(time_filename_list)])
+i_counter = 0
+diam_axis = pmc_data_nc.pmc_log_axis(min=1e-10,max=1e-6,n_bin=100)
+diam_axis_edges = diam_axis.edges()
+
+for [time, filename, key] in time_filename_list:
+    print time, filename, key
+    ncf = Scientific.IO.NetCDF.NetCDFFile(filename)
+    particles = pmc_data_nc.aero_particle_array_t(ncf)
+    ncf.close()
+
+    dry_diameter = particles.dry_diameter()
+    hist = pmc_data_nc.histogram_1d(dry_diameter, diam_axis, weights = particles.mass(include = ["SO4"]) / particles.comp_vol)
+    dist_array[i_counter,:] = hist
+    times[i_counter] = time
+    i_counter += 1
+
+np.savetxt("data/banana_so4_dist.txt", dist_array)
+np.savetxt("data/banana_diam.txt", diam_axis_edges)
+np.savetxt("data/banana_times.txt", times)
+    
+
+
+

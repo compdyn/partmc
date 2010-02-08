e39264e9bbdf7e937c3566964e925496e76d8e64
Matthew West <mwest@illinois.edu>
2010-02-08 21:13:18 +0000

    updated scenarios to point to executables in ../../build

--- scenarios/2_urban_plume2/run.ncoag.sh
+++ scenarios/2_urban_plume2/run.ncoag.sh
@@ -11,5 +11,5 @@ chemistry.
 ENDINFO
 sleep 1
 
-echo ../build/partmc urban_plume_no_coag.spec
-../build/partmc urban_plume_no_coag.spec
+echo ../../build/partmc urban_plume_no_coag.spec
+../../build/partmc urban_plume_no_coag.spec
--- scenarios/2_urban_plume2/run.wcoag.sh
+++ scenarios/2_urban_plume2/run.wcoag.sh
@@ -11,6 +11,6 @@ chemistry.
 ENDINFO
 sleep 1
 
-echo ../build/partmc urban_plume_with_coag.spec
-../build/partmc urban_plume_with_coag.spec
+echo ../../build/partmc urban_plume_with_coag.spec
+../../build/partmc urban_plume_with_coag.spec
 

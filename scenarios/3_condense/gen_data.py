8e6a2129e685ad29b54010b581d714206b76e0e1
Nicole Riemer <nriemer@illinois.edu>
2010-02-15 01:10:55 +0000

    deleted gen_data.py. superseeded by make_specs.py

--- scenarios/3_condense/gen_data.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-for h in [1, 7, 15, 24]:
-    print "%d hour => file %d with start time %d and temp times %d %d" \
-          % (h, 6 * h + 1, 3600 * h + 21600, 3600 * h, 3600 * h + 1200)

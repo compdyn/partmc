172fa7f0a1d7511e3773a888b99daaaa3558791e
Nicole Riemer <nriemer@illinois.edu>
2010-02-15 20:49:53 +0000

    added 5_weighted

---
+++ scenarios/5_weighted/3_run.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import subprocess
+
+str_exec = "../../build/partmc"
+
+for hour in range(1, 50):
+    print "hour = ", hour
+    
+    spec_file_ref = "spec/cond_%02d_ref.spec" % hour
+    spec_file_comp = "spec/cond_%02d_comp.spec" % hour 
+    spec_file_size = "spec/cond_%02d_size.spec" % hour
+    spec_file_both = "spec/cond_%02d_both.spec" % hour
+
+    command_1 = [str_exec, spec_file_ref]
+    command_2 = [str_exec, spec_file_comp]
+    command_3 = [str_exec, spec_file_size]
+    command_4 = [str_exec, spec_file_both]
+
+    print command_1
+    subprocess.check_call(command_1)
+
+    print command_2
+    subprocess.check_call(command_2)
+
+    print command_3
+    subprocess.check_call(command_3)
+
+    print command_4
+    subprocess.check_call(command_4)

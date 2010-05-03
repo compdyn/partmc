326722b59afb3ef6eab67ebfbd5ea055b0b82611
Matthew West <mwest@illinois.edu>
2010-05-03 20:03:59 +0000

    relaxed bounds on nucleate/run_test_2

--- test/nucleate/run_test_2.sh
+++ test/nucleate/run_test_2.sh
@@ -5,6 +5,6 @@ cd ${0%/*}
 
 echo "../../extract_aero_species out/nucleate_part_0001_ out/aero_species.txt"
 ../../extract_aero_species out/nucleate_part_0001_ out/aero_species.txt
-echo "../../numeric_diff out/aero_species.txt out/nucleate_ode_aero_mass.txt 0 5e-2 0 0 2 0"
-../../numeric_diff out/aero_species.txt out/nucleate_ode_aero_mass.txt 0 5e-2 0 0 2 0
+echo "../../numeric_diff out/aero_species.txt out/nucleate_ode_aero_mass.txt 0 0.1 0 0 2 0"
+../../numeric_diff out/aero_species.txt out/nucleate_ode_aero_mass.txt 0 0.1 0 0 2 0
 exit $?

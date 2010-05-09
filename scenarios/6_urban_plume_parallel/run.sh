9c58d1974b53f7f4893c179de1974be5a386ff07
Matthew West <mwest@illinois.edu>
2010-05-09 19:02:26 +0000

    added scenarios/6_urban_plume_parallel; works in parallel on teragrid/ncsa-abe

---
+++ scenarios/6_urban_plume_parallel/run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cat <<ENDINFO
+
+Urban Plume Test-case
+---------------------
+
+This simulates an idealized urban plume with gas and aerosol
+chemistry.
+
+ENDINFO
+sleep 1
+
+echo "../../build/partmc urban_plume_with_coag.spec"
+../../build/partmc urban_plume_with_coag.spec
+
+#echo "../../build/partmc urban_plume_no_coag.spec"
+#../../build/partmc urban_plume_no_coag.spec
+
+echo
+echo "Now run ./process.sh to process the data"
+echo

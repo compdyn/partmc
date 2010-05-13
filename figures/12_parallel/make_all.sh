dd51f63392644c7124142d0b96653b6b814816d3
Matthew West <mwest@illinois.edu>
2010-05-13 05:06:26 +0000

    added make_all script

---
+++ figures/12_parallel/make_all.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+./diam_bc_num_1_process.py
+./diam_bc_num_2_make_figs.py
+./diam_mass_1_process.py
+./diam_mass_2_make_figs.py
+./diam_num_1_process.py
+./diam_num_2_make_figs.py
+./diam_scrit_num_1_process.py
+./diam_scrit_num_2_make_figs.py
+./time_mass_1_process.py
+./time_mass_2_make_figs.py
+./time_num_1_process.py
+./time_num_2_make_figs.py
+./time_specmass_1_process.py
+./time_specmass_2_make_figs.py

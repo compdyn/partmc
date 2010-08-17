5afe80c1e45b81805487a491d8404662211295a5
Nicole Riemer <nriemer@illinois.edu>
2010-08-17 22:03:21 +0000

    added num_mass_diameter_1d_1_process.py

--- figures/13_paper_partmc_variable/num_mass_diameter_1d_1_process.py
+++ figures/13_paper_partmc_variable/num_mass_diameter_1d_1_process.py
@@ -9,7 +9,7 @@ import config
 
 i_loop_max = config.i_loop_max
 
-def make_plot(in_files, out_filename):
+def make_plot(in_files, f1, f2, f3):
     x_axis = partmc.log_grid(min=1e-10,max=1e-4,n_bin=100)
     x_centers = x_axis.centers()
     counter = 0
@@ -37,12 +37,9 @@ def make_plot(in_files, out_filename):
 for case in ["10K_wei+1", "10K_wei-1", "10K_wei-4" ]:
     for hour in range(12, 13):
         files = []
-        print 'files ', files
         for i_loop in range (0, config.i_loop_max):
             filename_in = "urban_plume_wc_%s_0%03d_000000%02d.nc" % (case, (i_loop+1), hour)
-            print i_loop, filename_in
             files.append(filename_in)
-        print files
         f1 = "data/1d_%s_%02d_x_values.txt" % (case, hour)
         f2 = "data/1d_%s_%02d_hist_array_gav.txt" % (case, hour)
         f3 = "data/1d_%s_%02d_e_bars.txt" % (case, hour)

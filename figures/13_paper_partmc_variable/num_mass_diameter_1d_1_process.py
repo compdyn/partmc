0575ddbab7c9b35e19e26d69100a217b1b18166f
Nicole Riemer <nriemer@illinois.edu>
2010-08-17 22:19:14 +0000

    made processing file for 1d size distributions

--- figures/13_paper_partmc_variable/num_mass_diameter_1d_1_process.py
+++ figures/13_paper_partmc_variable/num_mass_diameter_1d_1_process.py
@@ -9,30 +9,42 @@ import config
 
 i_loop_max = config.i_loop_max
 
-def make_plot(in_files, f1, f2, f3):
+def make_plot(in_files, f1, f2, f3, f4, f5):
     x_axis = partmc.log_grid(min=1e-10,max=1e-4,n_bin=100)
     x_centers = x_axis.centers()
     counter = 0
-    hist_array = np.zeros([len(x_centers),config.i_loop_max])
+    hist_array_num = np.zeros([len(x_centers),config.i_loop_max])
+    hist_array_mass = np.zeros([len(x_centers),config.i_loop_max])
     for file in in_files:
-        ncf = scipy.io.netcdf.netcdf_file(config.netcdf_dir+file, 'r')
+        ncf = scipy.io.netcdf.netcdf_file(config.netcdf_dir+'/'+file, 'r')
         particles = partmc.aero_particle_array_t(ncf)
         ncf.close()
 
         dry_diameters = particles.dry_diameters()
         hist = partmc.histogram_1d(dry_diameters, x_axis, weights = 1 / particles.comp_vols)
-        hist_array[:,counter] = hist
+        hist_array_num[:,counter] = hist
+        hist = partmc.histogram_1d(dry_diameters, x_axis, weights = particles.masses(exclude=["H2O"])  / particles.comp_vols)
+        hist_array_mass[:,counter] = hist
+
         counter = counter+1
 
-    hist_array_gav = np.exp(np.average(np.log(hist_array),axis = 1))
-    hist_array_gstd = np.exp(np.std(np.log(hist_array), axis = 1))
-    e_bar_top = hist_array_gav * hist_array_gstd
-    e_bar_bottom = hist_array_gav / hist_array_gstd
-    e_bars = np.vstack((hist_array_gav - e_bar_bottom, e_bar_top - hist_array_gav))
+    hist_array_gav_num = np.exp(np.average(np.log(hist_array_num),axis = 1))
+    hist_array_gstd_num = np.exp(np.std(np.log(hist_array_num), axis = 1))
+    e_bar_top_num = hist_array_gav_num * hist_array_gstd_num
+    e_bar_bottom_num = hist_array_gav_num / hist_array_gstd_num
+    e_bars_num = np.vstack((hist_array_gav_num - e_bar_bottom_num, e_bar_top_num - hist_array_gav_num))
+
+    hist_array_gav_mass = np.exp(np.average(np.log(hist_array_mass),axis = 1))
+    hist_array_gstd_mass = np.exp(np.std(np.log(hist_array_mass), axis = 1))
+    e_bar_top_mass = hist_array_gav_mass * hist_array_gstd_mass
+    e_bar_bottom_mass = hist_array_gav_mass / hist_array_gstd_mass
+    e_bars_mass = np.vstack((hist_array_gav_mass - e_bar_bottom_mass, e_bar_top_mass - hist_array_gav_mass))
 
     np.savetxt(f1, x_axis.centers())
-    np.savetxt(f2, hist_array_gav)
-    np.savetxt(f3, e_bars)
+    np.savetxt(f2, hist_array_gav_num)
+    np.savetxt(f3, e_bars_num)
+    np.savetxt(f4, hist_array_gav_mass)
+    np.savetxt(f5, e_bars_mass)
 
 for case in ["10K_wei+1", "10K_wei-1", "10K_wei-4" ]:
     for hour in range(12, 13):
@@ -41,10 +53,12 @@ for case in ["10K_wei+1", "10K_wei-1", "10K_wei-4" ]:
             filename_in = "urban_plume_wc_%s_0%03d_000000%02d.nc" % (case, (i_loop+1), hour)
             files.append(filename_in)
         f1 = "data/1d_%s_%02d_x_values.txt" % (case, hour)
-        f2 = "data/1d_%s_%02d_hist_array_gav.txt" % (case, hour)
-        f3 = "data/1d_%s_%02d_e_bars.txt" % (case, hour)
+        f2 = "data/1d_%s_%02d_hist_array_gav_num.txt" % (case, hour)
+        f3 = "data/1d_%s_%02d_e_bars_num.txt" % (case, hour)
+        f4 = "data/1d_%s_%02d_hist_array_gav_mass.txt" % (case, hour)
+        f5 = "data/1d_%s_%02d_e_bars_mass.txt" % (case, hour)
         print f1
-        make_plot(files, f1, f2, f3)
+        make_plot(files, f1, f2, f3, f4, f5)
 
 
 

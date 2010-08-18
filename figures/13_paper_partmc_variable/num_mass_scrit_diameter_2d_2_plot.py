4d64207ecd1aaf5621472312eac1b4132fa59000
Nicole Riemer <nriemer@illinois.edu>
2010-08-18 17:43:25 +0000

    added num_mass_scrit_diameter_2d_2_plot.py

--- figures/13_paper_partmc_variable/num_mass_scrit_diameter_2d_2_plot.py
+++ figures/13_paper_partmc_variable/num_mass_scrit_diameter_2d_2_plot.py
@@ -10,16 +10,16 @@ import partmc
 import mpl_helper
 import matplotlib
 
-x_array = np.loadtxt("data/2d_bc_10K_wei+1_12_x_values.txt") * 1e6
-y_array = np.loadtxt("data/2d_bc_10K_wei+1_12_y_values.txt") * 100
+x_array = np.loadtxt("data/2d_scrit_10K_wei+1_12_x_values.txt") * 1e6
+y_array = np.loadtxt("data/2d_scrit_10K_wei+1_12_y_values.txt") * 100
 
-num_avg1 = np.loadtxt("data/2d_bc_10K_wei+1_12_hist_array_average_num.txt") / 1e6
-num_avg2 = np.loadtxt("data/2d_bc_10K_wei-1_12_hist_array_average_num.txt") / 1e6
-num_avg3 = np.loadtxt("data/2d_bc_10K_wei-4_12_hist_array_average_num.txt") / 1e6
+num_avg1 = np.loadtxt("data/2d_scrit_10K_wei+1_12_hist_array_average_num.txt") / 1e6
+num_avg2 = np.loadtxt("data/2d_scrit_10K_wei-1_12_hist_array_average_num.txt") / 1e6
+num_avg3 = np.loadtxt("data/2d_scrit_10K_wei-4_12_hist_array_average_num.txt") / 1e6
 
-mass_avg1 = np.loadtxt("data/2d_bc_10K_wei+1_12_hist_array_average_mass.txt") * 1e9
-mass_avg2 = np.loadtxt("data/2d_bc_10K_wei-1_12_hist_array_average_mass.txt") * 1e9
-mass_avg3 = np.loadtxt("data/2d_bc_10K_wei-4_12_hist_array_average_mass.txt") * 1e9
+mass_avg1 = np.loadtxt("data/2d_scrit_10K_wei+1_12_hist_array_average_mass.txt") * 1e9
+mass_avg2 = np.loadtxt("data/2d_scrit_10K_wei-1_12_hist_array_average_mass.txt") * 1e9
+mass_avg3 = np.loadtxt("data/2d_scrit_10K_wei-4_12_hist_array_average_mass.txt") * 1e9
 
 (figure, axes_array) = mpl_helper.make_fig_array(3,2, figure_width=config.figure_width_double, 
                                                  left_margin=0.7, right_margin=0.6, vert_sep=0.3)
@@ -29,7 +29,7 @@ print 'num1 ', x_array, num_avg1, num_std1
 axes.pcolor(x_array, y_array, num_avg1.transpose(),linewidths = 0.1)
 axes.set_xscale("log")
 axes.set_yscale("linear")
-axes.set_ylabel(r"$w_{\rm BC} / \%$")
+axes.set_ylabel(r"$S_{\rm crit} / \%$")
 axes.set_ylim(0, 0.8)
 axes.set_xlim(5e-3, 5)
 axes.grid(True)
@@ -38,7 +38,7 @@ axes = axes_array[1][0]
 axes.pcolor(x_array, y_array, num_avg2.transpose(),linewidths = 0.1)
 axes.set_xscale("log")
 axes.set_yscale("linear")
-axes.set_ylabel(r"$w_{\rm BC} / %$")
+axes.set_ylabel(r"$S_{\rm crit} / \%$")
 axes.set_ylim(0, 0.8)
 axes.set_xlim(5e-3, 5)
 axes.grid(True)
@@ -47,7 +47,7 @@ axes = axes_array[0][0]
 axes.pcolor(x_array, y_array, num_avg3.transpose(),linewidths = 0.1)
 axes.set_xscale("log")
 axes.set_yscale("linear")
-axes.set_ylabel(r"$w_{\rm BC} / %$")
+axes.set_ylabel(r"$S_{\rm crit} / \%$")
 axes.set_ylim(0, 0.8)
 axes.set_xlim(5e-3, 5)
 axes.grid(True)
@@ -57,7 +57,7 @@ axes = axes_array[2][1]
 axes.pcolor(x_array, y_array, mass_avg1.transpose(),linewidths = 0.1)
 axes.set_xscale("log")
 axes.set_yscale("linear")
-axes.set_ylabel(r"$w_{\rm BC} / %$")
+axes.set_ylabel(r"$S_{\rm crit} / \%$")
 axes.set_ylim(0, 0.8)
 axes.set_xlim(5e-3, 5)
 axes.grid(True)
@@ -66,7 +66,7 @@ axes = axes_array[1][1]
 axes.pcolor(x_array, y_array, mass_avg2.transpose(),linewidths = 0.1)
 axes.set_xscale("log")
 axes.set_yscale("linear")
-axes.set_ylabel(r"$w_{\rm BC} / %$")
+axes.set_ylabel(r"$S_{\rm crit} / \%$")
 axes.set_ylim(0, 0.8)
 axes.set_xlim(5e-3, 5)
 axes.grid(True)
@@ -75,7 +75,7 @@ axes = axes_array[0][1]
 axes.pcolor(x_array, y_array, mass_avg3.transpose(),linewidths = 0.1)
 axes.set_xscale("log")
 axes.set_yscale("linear")
-axes.set_ylabel(r"$w_{\rm BC}$")
+axes.set_ylabel(r"$S_{\rm crit} / \%$")
 axes.set_ylim(0, 0.8)
 axes.set_xlim(5e-3, 5)
 axes.grid(True)
@@ -83,5 +83,5 @@ axes.set_xlabel(r"diameter / $\rm \mu m$")
 
 mpl_helper.remove_fig_array_axes(axes_array)
 
-figure.savefig("figs/num_mass_bc_diameter_2d.pdf")
+figure.savefig("figs/num_mass_scrit_diameter_2d.pdf")
 

e197f3d9a666cd7bf8d3f03b941196d10e3dea00
Nicole Riemer <nriemer@illinois.edu>
2010-08-18 20:44:12 +0000

    added cv_scav_bc_cv_ccn.py

--- figures/13_paper_partmc_variable/cv_scav_bc_cv_ccn.py
+++ figures/13_paper_partmc_variable/cv_scav_bc_cv_ccn.py
@@ -10,79 +10,70 @@ import partmc
 import mpl_helper
 import matplotlib
 
+bc_std_overall = np.zeros([12,4]) # 12 for weighting schemes, 4 for ss-values
+ccn_std_overall = np.zeros([12,4])
+
 i_counter = 0
-i_counter_p = 0
-array_num_std_avg = np.zeros([3,7]) # 3 for particle number cases, 7 for weighting cases
-array_mass_std_avg = np.zeros([3,7])
-
-num_avg = np.zeros([3,7,25])
-num_std = np.zeros([3,7,25])
-mass_avg = np.zeros([3,7,25])
-mass_std = np.zeros([3,7,25])
-
-for counter_p in ["1K", "10K", "100K"]:
-    print counter_p, i_counter_p
-    i_counter = 0
-    for counter in ["wei\+1", "flat", "wei-1", "wei-2", "wei-3", "wei-4", "mfa"]:
-        print counter, i_counter
-        f1 = "data/average_number_%s_%s.txt" % (counter_p, counter)
-        f2 = "data/std_number_%s_%s.txt" % (counter_p, counter)
-        f3 = "data/average_mass_%s_%s.txt" % (counter_p, counter)
-        f4 = "data/std_mass_%s_%s.txt" % (counter_p, counter)
-
-        print f1, f2, f3, f4
-        num_avg[i_counter_p,i_counter,:] = np.loadtxt(f1)
-        num_std[i_counter_p,i_counter,:] = np.loadtxt(f2)
-        mass_avg[i_counter_p,i_counter,:] = np.loadtxt(f3)
-        mass_std[i_counter_p,i_counter,:] = np.loadtxt(f4)
-
-# average std over time
-
-        array_num_std_avg[i_counter_p,i_counter] = np.average(num_std[i_counter_p,i_counter,:] / 
-                                                              num_avg[i_counter_p,i_counter,:])
-        array_mass_std_avg[i_counter_p,i_counter] = np.average(mass_std[i_counter_p, i_counter,:] / 
-                                                               mass_avg[i_counter_p, i_counter,:])
-        i_counter = i_counter + 1
-    i_counter_p = i_counter_p + 1
-
-print 'array_num_std_avg ', array_num_std_avg
-print 'mfa ', array_num_std_avg[0,6], array_mass_std_avg[0,6]
-x_array = [1, 0, -1, -2, -3, -4]
+for counter in ["ss1", "ss2", "ss3", "ss4"]:
+    print "counter ", i_counter, counter
+    print "ccn_std_overall", ccn_std_overall
+    f1 = "data/ccn_std_overall_%s.txt" % counter
+    f2 = "data/bc_std_overall_%s.txt" % counter
+    print "f1 ", f1
+    print "content ", np.loadtxt(f1)
+    ccn_std_overall[:,i_counter] = np.loadtxt(f1)
+    bc_std_overall[:,i_counter] = np.loadtxt(f2)
+    i_counter += 1
 
 (figure, axes) = mpl_helper.make_fig(figure_width=config.figure_width_single)
-
 axes.set_xscale("log")
 axes.set_yscale("log")
-axes.plot(array_num_std_avg[0,0:6], array_mass_std_avg[0,0:6], 'xr-', label = '1K')
-#axes.plot(array_num_std_avg[0,6], array_mass_std_avg[0,6], 'or', label = '1K mfa')
-axes.plot(array_num_std_avg[1,0:6], array_mass_std_avg[1,0:6], 'xg-', label = '10K')
-#axes.plot(array_num_std_avg[1,6], array_mass_std_avg[1,6], 'og', label = '10K mfa')
-axes.plot(array_num_std_avg[2,0:6], array_mass_std_avg[2,0:6], 'xb-', label = '100K')
-#axes.plot(array_num_std_avg[2,6], array_mass_std_avg[2,6], 'ob', label = '100K mfa')
+axes.set_xlabel("CV(xxx) ")
+axes.set_ylabel("CV(yyy)")
 axes.grid()
-axes.set_xlabel(r"${\rm CV}(\overline{N(t)})$")
-axes.set_ylabel(r"${\rm CV}(\overline{M(t)})$")
+axes.plot(bc_std_overall[0:6,1], ccn_std_overall[0:6,1],'g-x', label = '1K S_c = 0.01%')
+axes.plot(bc_std_overall[0:6,2], ccn_std_overall[0:6,2], 'b-x', label = '1K S_c = 0.1%')
+axes.plot(bc_std_overall[0:6,3], ccn_std_overall[0:6,3], 'm-x', label = '1K S_c = 0.5%')
 
+#axes.plot(bc_std_overall[18,1], ccn_std_overall[18,1], 'go', label = '1K S_c = 0.01% mfa')
+#axes.plot(bc_std_overall[18,2], ccn_std_overall[18,2], 'bo', label = '1K S_c = 0.1% mfa')
+#axes.plot(bc_std_overall[18,3], ccn_std_overall[18,3], 'mo', label = '1K S_c = 0.5% mfa')
 
-mpl_helper.label_plot_line(axes, array_num_std_avg[0,0:6], array_mass_std_avg[0,0:6] , 0.03, "1K",
-                                  verticalalignment="bottom", horizontalalignment="right")
-mpl_helper.label_plot_line(axes, array_num_std_avg[1,0:6], array_mass_std_avg[1,0:6] , 0.01, "10K",
-                                  verticalalignment="bottom", horizontalalignment="right")
-mpl_helper.label_plot_line(axes, array_num_std_avg[2,0:6], array_mass_std_avg[2,0:6] , 0.002, "100K",
-                                  verticalalignment="bottom", horizontalalignment="right")
+axes.plot(bc_std_overall[6:12,1], ccn_std_overall[6:12,1], 'g--x', label = '10K S_c = 0.01%')
+axes.plot(bc_std_overall[6:12,2], ccn_std_overall[6:12,2], 'b--x', label = '10K S_c = 0.1%')
+axes.plot(bc_std_overall[6:12,3], ccn_std_overall[6:12,3], 'm--x', label = '10K S_c = 0.5%')
 
-mpl_helper.label_plot_line(axes, array_num_std_avg[0,0:6], array_mass_std_avg[0,0:6] , 0.03, "1",
-                                  verticalalignment="bottom", horizontalalignment="left")
-mpl_helper.label_plot_line(axes, array_num_std_avg[0,0:6], array_mass_std_avg[0,0:6] , 0.02, "0",
-                                  verticalalignment="bottom", horizontalalignment="left")
-mpl_helper.label_plot_line(axes, array_num_std_avg[0,0:6], array_mass_std_avg[0,0:6] , 0.04, "-1",
-                                  verticalalignment="bottom", horizontalalignment="left")
-mpl_helper.label_plot_line(axes, array_num_std_avg[0,0:6], array_mass_std_avg[0,0:6] , 0.05, "-2",
-                                  verticalalignment="bottom", horizontalalignment="left")
-mpl_helper.label_plot_line(axes, array_num_std_avg[0,0:6], array_mass_std_avg[0,0:6] , 0.06, "-3",
-                                  verticalalignment="bottom", horizontalalignment="left")
-mpl_helper.label_plot_line(axes, array_num_std_avg[0,0:6], array_mass_std_avg[0,0:6] , 0.07, "-4",
-                                  verticalalignment="bottom", horizontalalignment="left")
+#axes.plot(bc_std_overall[19,1], ccn_std_overall[19,1], 'go', label = '10K S_c = 0.01% mfa')
+#axes.plot(bc_std_overall[19,2], ccn_std_overall[19,2], 'bo', label = '10K S_c = 0.1% mfa')
+#axes.plot(bc_std_overall[19,3], ccn_std_overall[19,3], 'mo', label = '10K S_c = 0.5% mfa')
 
-figure.savefig("figs/cv_mass_cv_num.pdf")
+#axes.plot(bc_std_overall[12:18,1], ccn_std_overall[12:18,1], 'g--', label = '100K S_c = 0.01%')
+#axes.plot(bc_std_overall[12:18,2], ccn_std_overall[12:18,2], 'b--', label = '100K S_c = 0.1%')
+#axes.plot(bc_std_overall[12:18,3], ccn_std_overall[12:18,3], 'm--', label = '100K S_c = 0.5%')
+
+#axes.plot(bc_std_overall[20,1], ccn_std_overall[20,1], 'go', label = '100K S_c = 0.01% mfa')
+#axes.plot(bc_std_overall[20,2], ccn_std_overall[20,2], 'bo', label = '100K S_c = 0.1% mfa')
+#axes.plot(bc_std_overall[20,3], ccn_std_overall[20,3], 'mo', label = '100K S_c = 0.5% mfa')
+
+#mpl_helper.label_plot_line(axes, bc_std_overall[0,0:6], ccn_std_overall[0,0:6] , 0.03, "1K",
+#                                  verticalalignment="bottom", horizontalalignment="right")
+#mpl_helper.label_plot_line(axes, bc_std_overall[1,0:6], ccn_std_overall[1,0:6] , 0.01, "10K",
+#                                  verticalalignment="bottom", horizontalalignment="right")
+#mpl_helper.label_plot_line(axes, bc_std_overall[2,0:6], ccn_std_overall[2,0:6] , 0.002, "100K",
+#                                  verticalalignment="bottom", horizontalalignment="right")
+
+#mpl_helper.label_plot_line(axes, bc_std_overall[0,0:6], ccn_std_overall[0,0:6] , 0.03, "1",
+#                                  verticalalignment="bottom", horizontalalignment="left")
+#mpl_helper.label_plot_line(axes, bc_std_overall[0,0:6], ccn_std_overall[0,0:6] , 0.02, "0",
+#                                  verticalalignment="bottom", horizontalalignment="left")
+#mpl_helper.label_plot_line(axes, bc_std_overall[0,0:6], ccn_std_overall[0,0:6] , 0.04, "-1",
+#                                  verticalalignment="bottom", horizontalalignment="left")
+#mpl_helper.label_plot_line(axes, bc_std_overall[0,0:6], ccn_std_overall[0,0:6] , 0.05, "-2",
+#                                  verticalalignment="bottom", horizontalalignment="left")
+#mpl_helper.label_plot_line(axes, bc_std_overall[0,0:6], ccn_std_overall[0,0:6] , 0.06, "-3",
+#                                  verticalalignment="bottom", horizontalalignment="left")
+#mpl_helper.label_plot_line(axes, bc_std_overall[0,0:6], ccn_std_overall[0,0:6] , 0.07, "-4",
+#                                  verticalalignment="bottom", horizontalalignment="left")
+
+figure.savefig("figs/cv_scav_bc_cv_ccn.pdf")
 

0af506a81f8f8c195476b1bc279ccf39abeb8d1c
Matthew West <mwest@illinois.edu>
2010-08-19 17:05:56 +0000

    new colorbar grid layout; num_mass_bc_scrit_diameter_compo_2d done

--- figures/13_paper_partmc_variable/num_mass_bc_scrit_diameter_compo_2d_2_plot.py
+++ figures/13_paper_partmc_variable/num_mass_bc_scrit_diameter_compo_2d_2_plot.py
@@ -23,9 +23,9 @@ mass_bc = np.loadtxt("data/2d_bc_compo_12_average_mass.txt") * 1e9
 mass_scrit = np.loadtxt("data/2d_scrit_compo_12_average_mass.txt") * 1e9
 
 (figure, axes_array, cbar_axes_array) = mpl_helper.make_fig_array(2,2, figure_width=config.figure_width_double, 
-                                                                  left_margin=0.7, right_margin=1.6,
-                                                                  vert_sep=0.3, horiz_sep=0.8,
-                                                                  share_y_axes=False, colorbar=True)
+                                                                  left_margin=0.7, right_margin=0.1,
+                                                                  top_margin=0.8, vert_sep=1.5, horiz_sep=0.3,
+                                                                  colorbar="individual", colorbar_location="top")
 
 axes = axes_array[1][0]
 cbar_axes = cbar_axes_array[1][0]
@@ -34,52 +34,58 @@ axes.set_xscale("log")
 axes.set_yscale("linear")
 axes.set_ylabel(r"BC mass frac. $w_{\rm BC}$ / \%")
 axes.set_ylim(0, 80)
+axes.set_xlabel(r"diameter $D$ / $\rm \mu m$")
 axes.grid(True)
-figure.colorbar(p, cax=cbar_axes, format=matplotlib.ticker.LogFormatterMathtext())
-cbar_axes.set_ylabel(r"$n(D,w)$ / $\rm cm^{-3}$")
+cbar = figure.colorbar(p, cax=cbar_axes, format=matplotlib.ticker.LogFormatterMathtext(),
+                       orientation='horizontal')
+cbar_axes.xaxis.set_label_position('top')
+cbar.set_label(r"num. conc. $n(D,w)$ / $\rm cm^{-3}$")
+cbar.set_ticks([1e-3, 1e-1, 1e1, 1e3, 1e5])
 
 axes = axes_array[0][0]
 cbar_axes = cbar_axes_array[0][0]
-p = axes.pcolor(x_array_bc, y_array_bc, mass_bc.transpose(),linewidths = 0.1, norm=matplotlib.colors.LogNorm())
-axes.set_xscale("log")
-axes.set_yscale("linear")
-axes.set_ylabel(r"BC mass frac. $w_{\rm BC}$ / \%")
-axes.set_ylim(0, 80)
-axes.set_xlim(5e-3, 5)
-axes.set_xlabel(r"diameter $D$ / $\rm \mu m$")
-axes.grid(True)
-figure.colorbar(p, cax=cbar_axes, format=matplotlib.ticker.LogFormatterMathtext())
-cbar_axes.set_ylabel(r"$m(D,w)$ / $\rm \mu g \ m^{-3}$")
-
-axes = axes_array[1][1]
-cbar_axes = cbar_axes_array[1][1]
 p = axes.pcolor(x_array_scrit, y_array_scrit, num_scrit.transpose(),linewidths = 0.1, norm=matplotlib.colors.LogNorm())
 axes.set_xscale("log")
 axes.set_yscale("log")
-axes.yaxis.tick_right()
-axes.yaxis.set_label_position('right')
 axes.set_ylabel(r"crit. supersat. $S_{\rm crit}$ / \%")
 axes.set_ylim(1e-3, 1e2)
+axes.set_xlim(5e-3, 5)
+axes.set_xlabel(r"diameter $D$ / $\rm \mu m$")
 axes.grid(True)
-figure.colorbar(p, cax=cbar_axes, format=matplotlib.ticker.LogFormatterMathtext())
-cbar_axes.set_ylabel(r"$n(D,S)$ / $\rm cm^{-3}$")
+cbar = figure.colorbar(p, cax=cbar_axes, format=matplotlib.ticker.LogFormatterMathtext(),
+                       orientation='horizontal')
+cbar_axes.xaxis.set_label_position('top')
+cbar.set_label(r"num. conc. $n(D,S)$ / $\rm cm^{-3}$")
+cbar.set_ticks([1e-3, 1e-1, 1e1, 1e3, 1e5])
+
+axes = axes_array[1][1]
+cbar_axes = cbar_axes_array[1][1]
+p = axes.pcolor(x_array_bc, y_array_bc, mass_bc.transpose(),linewidths = 0.1, norm=matplotlib.colors.LogNorm())
+axes.set_xscale("log")
+axes.set_yscale("linear")
+axes.set_xlabel(r"diameter $D$ / $\rm \mu m$")
+axes.grid(True)
+cbar = figure.colorbar(p, cax=cbar_axes, format=matplotlib.ticker.LogFormatterMathtext(),
+                       orientation='horizontal')
+cbar_axes.xaxis.set_label_position('top')
+cbar.set_label(r"mass conc. $m(D,w)$ / $\rm \mu g \ m^{-3}$")
+cbar.set_ticks([1e-7, 1e-5, 1e-3, 1e-1, 1e1])
 
 axes = axes_array[0][1]
 cbar_axes = cbar_axes_array[0][1]
 p = axes.pcolor(x_array_scrit, y_array_scrit, mass_scrit.transpose(),linewidths = 0.1, norm=matplotlib.colors.LogNorm())
 axes.set_xscale("log")
 axes.set_yscale("log")
-axes.yaxis.tick_right()
-axes.yaxis.set_label_position('right')
-axes.set_ylabel(r"crit. supersat. $S_{\rm crit}$ / \%")
-axes.set_ylim(1e-3, 1e2)
 axes.set_xlim(5e-3, 5)
 axes.set_xlabel(r"diameter $D$ / $\rm \mu m$")
 axes.grid(True)
-figure.colorbar(p, cax=cbar_axes, format=matplotlib.ticker.LogFormatterMathtext())
-cbar_axes.set_ylabel(r"$m(D,S)$ / $\rm \mu g \ m^{-3}$")
+cbar = figure.colorbar(p, cax=cbar_axes, format=matplotlib.ticker.LogFormatterMathtext(),
+                       orientation='horizontal')
+cbar_axes.xaxis.set_label_position('top')
+cbar.set_label(r"mass conc. $m(D,S)$ / $\rm \mu g \ m^{-3}$")
+cbar.set_ticks([1e-7, 1e-5, 1e-3, 1e-1, 1e1])
 
-mpl_helper.remove_fig_array_axes(axes_array, remove_y_axes=False)
+mpl_helper.remove_fig_array_axes(axes_array, remove_x_axes=False)
 
 figure.savefig("figs/num_mass_bc_scrit_diameter_compo_2d.pdf")
 
--- tool/mpl_helper.py
+++ tool/mpl_helper.py
@@ -68,7 +68,8 @@ def make_fig_array(n_vert=2,
                    top_margin=0.2,
                    horiz_sep=0.4,
                    vert_sep=0.4,
-                   colorbar=False,
+                   colorbar_location="left", # "left", "right", "top", "bottom"
+                   colorbar=None, # "individual", "single", "shared"
                    top_colorbar=False,
                    colorbar_width=0.15,
                    colorbar_length_fraction=0.8,
@@ -77,12 +78,25 @@ def make_fig_array(n_vert=2,
                    share_y_axes=True,
                    ):
     """
+    (figure, axes_array) = make_fig_array()
+    (figure, axes_array, cbar_axes_array) = make_fig_array(colorbar="individual")
+    (figure, axes_array, cbar_axes_array) = make_fig_array(colorbar="shared")
+    (figure, axes_array, cbar_axes) = make_fig_array(colorbar="single")
+
     Numbering convention:
-    axes_array[i][j] has (i,j):
+    axes_array[i][j] has (i,j) layout:
 
           (2,0) (2,1)
           (1,0) (1,1)
           (0,0) (0,1)
+
+    If colorbar is "individual" then cbar_axes_array has the same
+    dimensions and format as axes_array.
+
+    If colorbar is "shared" then cbar_axes_array is a 1D list of
+    colorbars.
+
+    If colorbar is "single" then only one cbar_axes is returned.
     """
     axis_width = (figure_width - left_margin - right_margin - (n_horiz - 1) * horiz_sep) / float(n_horiz)
     axis_height = axis_width / axis_ratio
@@ -111,36 +125,109 @@ def make_fig_array(n_vert=2,
                 last_y_axes = new_axes
             if i_vert == 0:
                 last_x_axes = new_axes
-    if colorbar:
-        colorbar_axes_array = []
+    if colorbar in ["individual", "shared"]:
+        cbar_axes_array = []
         for i_vert in range(n_vert):
-            colorbar_axes_array.append([])
+            if colorbar == "individual":
+                cbar_axes_array.append([])
             for i_horiz in range(n_horiz):
-                x_left = left_margin + (i_horiz + 1) * axis_width \
-                    + i_horiz * horiz_sep + colorbar_offset
-                y_bottom = bottom_margin \
-                    + i_vert * (axis_height + vert_sep) \
-                    + axis_height * (1.0 - colorbar_length_fraction) / 2.0
-                new_axes = figure.add_axes([x_left / figure_width,
-                                            y_bottom / figure_height,
-                                            colorbar_width / figure_width,
-                                            axis_height * colorbar_length_fraction / figure_height])
-                colorbar_axes_array[-1].append(new_axes)
-        return (figure, axes_array, colorbar_axes_array)
-    if top_colorbar:
-        colorbar_axes_array = []
-        for i_horiz in range(n_horiz):
-            x_left = left_margin + i_horiz * (axis_width + horiz_sep) \
-                + axis_width * (1.0 - colorbar_length_fraction) / 2.0
+                if colorbar_location == "left":
+                    x_left = left_margin + i_horiz * (axis_width + horiz_sep) \
+                        - colorbar_offset - colorbar_width
+                    y_bottom = bottom_margin \
+                        + i_vert * (axis_height + vert_sep) \
+                        + axis_height * (1.0 - colorbar_length_fraction) / 2.0
+                    x_width = colorbar_width
+                    y_height = axis_height * colorbar_length_fraction
+                elif colorbar_location == "right":
+                    x_left = left_margin + (i_horiz + 1) * axis_width \
+                        + i_horiz * horiz_sep + colorbar_offset
+                    y_bottom = bottom_margin \
+                        + i_vert * (axis_height + vert_sep) \
+                        + axis_height * (1.0 - colorbar_length_fraction) / 2.0
+                    x_width = colorbar_width
+                    y_height = axis_height * colorbar_length_fraction
+                elif colorbar_location == "bottom":
+                    x_left = left_margin + i_horiz * (axis_width + horiz_sep) \
+                        + axis_width * (1.0 - colorbar_length_fraction) / 2.0
+                    y_bottom = bottom_margin \
+                        + i_vert * (axis_height + vert_sep) \
+                        - colorbar_offset - colorbar_width
+                    x_width = axis_width * colorbar_length_fraction
+                    y_height = colorbar_width
+                elif colorbar_location == "top":
+                    x_left = left_margin + i_horiz * (axis_width + horiz_sep) \
+                        + axis_width * (1.0 - colorbar_length_fraction) / 2.0
+                    y_bottom = bottom_margin \
+                        + (i_vert + 1) * axis_height + i_vert * vert_sep \
+                        + colorbar_offset
+                    x_width = axis_width * colorbar_length_fraction
+                    y_height = colorbar_width
+                else:
+                    raise Exception("unknown colorbar: %s" % str(colorbar))
+                if colorbar == "individual" \
+                        or (colorbar == "shared"
+                            and ((colorbar_location == "left" and i_horiz == 0)
+                                 or (colorbar_location == "right" and i_horiz == n_horiz - 1)
+                                 or (colorbar_location == "bottom" and i_vert == 0)
+                                 or (colorbar_location == "top" and i_vert == n_vert - 1))):
+                        cbar_axes = figure.add_axes([x_left / figure_width,
+                                                     y_bottom / figure_height,
+                                                     x_width / figure_width,
+                                                     y_height / figure_height])
+                        if colorbar_location == "left":
+                            cbar_axes.xaxis.tick_left()
+                            cbar_axes.xaxis.set_label_position('left')
+                        if colorbar_location == "top":
+                            cbar_axes.xaxis.tick_top()
+                            cbar_axes.xaxis.set_label_position('top')
+                if colorbar == "individual":
+                    cbar_axes_array[-1].append(cbar_axes)
+                elif colorbar == "shared":
+                    cbar_axes_array.append(cbar_axes)
+        return (figure, axes_array, cbar_axes_array)
+    elif colorbar == "single":
+        total_width = n_horiz * axis_width + (n_horiz - 1) * horiz_sep
+        total_height = n_vert * axis_height + (n_vert - 1) * vert_sep
+        if colorbar_location == "left":
+            x_left = left_margin - colorbar_offset - colorbar_width
             y_bottom = bottom_margin \
-                + n_vert * axis_height + (n_vert - 1) * vert_sep \
-                + colorbar_offset
-            new_axes = figure.add_axes([x_left / figure_width,
-                                        y_bottom / figure_height,
-                                        axis_width * colorbar_length_fraction / figure_width,
-                                        colorbar_width / figure_height])
-            colorbar_axes_array.append(new_axes)
-        return (figure, axes_array, colorbar_axes_array)
+                + total_height * (1.0 - colorbar_length_fraction) / 2.0
+            x_width = colorbar_width
+            y_height = total_height * colorbar_length_fraction
+        elif colorbar_location == "right":
+            x_left = left_margin + total_width + colorbar_offset
+            y_bottom = bottom_margin \
+                + total_height * (1.0 - colorbar_length_fraction) / 2.0
+            x_width = colorbar_width
+            y_height = total_height * colorbar_length_fraction
+        elif colorbar_location == "bottom":
+            x_left = left_margin \
+                + total_width * (1.0 - colorbar_length_fraction) / 2.0
+            y_bottom = bottom_margin - colorbar_offset - colorbar_width
+            x_width = total_width * colorbar_length_fraction
+            y_height = colorbar_width
+        elif colorbar_location == "top":
+            x_left = left_margin \
+                + total_width * (1.0 - colorbar_length_fraction) / 2.0
+            y_bottom = bottom_margin + total_height + colorbar_offset
+            x_width = total_width * colorbar_length_fraction
+            y_height = colorbar_width
+        else:
+            raise Exception("unknown colorbar: %s" % str(colorbar))
+        cbar_axes = figure.add_axes([x_left / figure_width,
+                                     y_bottom / figure_height,
+                                     x_width / figure_width,
+                                     y_height / figure_height])
+        if colorbar_location == "left":
+            cbar_axes.xaxis.tick_left()
+            cbar_axes.xaxis.set_label_position('left')
+        if colorbar_location == "top":
+            cbar_axes.xaxis.tick_top()
+            cbar_axes.xaxis.set_label_position('top')
+        return (figure, axes_array, cbar_axes)
+    elif colorbar is not None:
+        raise Exception("unknown colorbar: %s" % str(colorbar))
     return (figure, axes_array)
 
 def remove_fig_array_axes(axes_array, remove_x_axes=True, remove_y_axes=True):

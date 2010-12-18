490ed404922660c42667d2e5b32922e83fc03e2c
Nicole Riemer <nriemer@illinois.edu>
2010-12-18 15:43:28 +0000

    added py script to make number concentration over coating thickness / size

---
+++ figures/7_condensation/fig_1d_coating.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import scipy.io
+import sys
+import math
+import numpy as np
+import matplotlib
+matplotlib.use("PDF")
+import matplotlib.pyplot as plt
+sys.path.append("../../tool")
+import partmc
+
+def make_plot(in_filename,out_filename,title):
+    ncf = scipy.io.netcdf.netcdf_file(in_filename, 'r')
+    particles = partmc.aero_particle_array_t(ncf)
+    ncf.close()
+
+    bc_dens = particles.aero_data.densities[18]
+    bc = particles.masses(include = ["BC"])
+    dry_mass = particles.masses(exclude = ["H2O"])
+    bc_frac = bc / dry_mass
+    coat_frac = 1 - bc / dry_mass
+
+    is_bc = (bc_frac > 0.05)
+    dry_diameters = particles.dry_diameters()
+
+    core_diameters = (6 / math.pi * bc / bc_dens)**(1./3.)
+    coating_thickness = (dry_diameters - core_diameters) / 2.
+    ratio = coating_thickness/dry_diameters
+
+    print ratio.max()
+    x_axis = partmc.linear_grid(min=0,max=ratio.max(),n_bin=50)
+
+    hist1d = partmc.histogram_1d(coating_thickness[is_bc]/dry_diameters[is_bc], x_axis, weights = 1/particles.comp_vols[is_bc])
+
+    plt.clf()
+    a = plt.gca()
+    a.set_xscale("linear")
+    a.set_yscale("log")
+    plt.plot(x_axis.centers(), hist1d)
+    plt.axis([x_axis.min, x_axis.max, 1e8, 1e12])
+    plt.xlabel("coating thickness / dry diameter")
+    plt.ylabel("number density (m^{-3})")
+    plt.title(title)
+    fig = plt.gcf()
+    fig.savefig(out_filename)
+
+for hour in range(1, 24):
+    minutes = hour * 60. 
+    print "hour = ", hour
+    filename_in1 = "../../scenarios/2_urban_plume2/out/urban_plume_wc_0001_0000%04d.nc" % minutes
+    filename_out1 = "figs/2d_bc_urban_plume_%02d.pdf" % (hour-1)
+    titel = "%02d hours" % (hour-1)
+    print filename_in1
+    print filename_out1
+    print titel
+
+    make_plot(filename_in1, filename_out1, titel)
+#    make_plot(filename_in2, filename_out2, titel)
+#    make_plot(filename_in3, filename_out3, titel)
+#    make_plot(filename_in4, filename_out4, titel)
+
+

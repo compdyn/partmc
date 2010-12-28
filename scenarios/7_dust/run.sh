57378323763e75504e3c5c2b41b49162e3f4d211
Matthew West <mwest@illinois.edu>
2010-12-28 20:50:04 +0000

    fix 7_dust/run* scripts

--- scenarios/7_dust/run.sh
+++ scenarios/7_dust/run.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-cat <<ENDINFO
+# exit on error
+set -e
+# turn on command echoing
+set -v
 
-Urban Plume Test-case
----------------------
+mkdir -p out
 
-This simulates an idealized urban plume with gas and aerosol
-chemistry.
-
-ENDINFO
-sleep 1
-
-echo ../../build/partmc urban_plume_with_coag.spec
 ../../build/partmc urban_plume_with_coag.spec
-echo ../../build/partmc urban_plume_no_coag.spec
 ../../build/partmc urban_plume_no_coag.spec

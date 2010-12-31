c433c1c4cb9dfd1c71cef666ab3192f986aa0474
Matthew West <mwest@illinois.edu>
2010-12-31 20:45:02 +0000

    clean up test/parallel dist_single case

--- test/parallel/run_test_4.sh
+++ test/parallel/run_test_4.sh
@@ -13,11 +13,6 @@ mpirun -v -np 10 ../../partmc run_part_parallel_dist_single.spec
 ../../extract_aero_total out/parallel_dist_single_0001_ out/parallel_dist_single_aero_total.txt
 
 ../../numeric_diff out/sect_aero_size_num.txt out/parallel_dist_single_aero_size_num.txt 0 0.3 0 0 2 0
-R1=$?
 ../../numeric_diff out/sect_aero_size_mass.txt out/parallel_dist_single_aero_size_mass.txt 0 0.3 0 0 2 0
-R2=$?
 ../../numeric_diff out/sect_aero_total.txt out/parallel_dist_single_aero_total.txt 0 0.3 0 0 2 2
-R3=$?
 ../../numeric_diff out/sect_aero_total.txt out/parallel_dist_single_aero_total.txt 0 0.3 0 0 3 3
-R4=$?
-exit $(( $R1 || $R2 || $R3 || $R4 ))

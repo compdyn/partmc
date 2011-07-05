e1f88ce994dd900e17a880b9c9b1740351ade464
Nicole Riemer <nriemer@illinois.edu>
2011-07-05 15:11:42 +0000

    developed processing script for kinetic effects and plotting script

---
+++ figures/7_condensation/kinetic_effects_plot.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python
+
+import scipy.io
+import sys
+import numpy as np
+
+sys.path.append("../../tool")
+import partmc
+import matplotlib
+import mpl_helper
+import config
+#matplotlib.use("PDF")
+#import matplotlib.pyplot as plt
+
+
+seconds = np.loadtxt("seconds.txt")
+rh = np.loadtxt("rh.txt")
+
+diam_not_activate = np.loadtxt("diam_not_activate.txt")
+diam_evaporation = np.loadtxt("diam_evaporation.txt")
+diam_deactivation = np.loadtxt("diam_deactivation.txt")
+diam_inertial = np.loadtxt("diam_inertial.txt")
+rh_eq_not_activate = np.loadtxt("rh_eq_not_activate.txt")
+rh_eq_evaporation = np.loadtxt("rh_eq_evaporation.txt")
+rh_eq_deactivation = np.loadtxt("rh_eq_deactivation.txt")
+rh_eq_inertial = np.loadtxt("rh_eq_inertial.txt")
+     
+rh_c_not_activate = np.loadtxt("rh_c_not_activate.txt")
+rh_c_evaporation =  np.loadtxt("rh_c_evaporation.txt")
+rh_c_deactivation = np.loadtxt("rh_c_deactivation.txt")
+rh_c_inertial =  np.loadtxt("rh_c_inertial.txt")
+d_c_not_activate = np.loadtxt("d_c_not_activate.txt")
+d_c_evaporation = np.loadtxt("d_c_evaporation.txt")
+d_c_deactivation = np.loadtxt("d_c_deactivation.txt")
+d_c_inertial = np.loadtxt("d_c_inertial.txt")
+
+wet_diameter_not_activate = np.loadtxt("wet_diameters_not_activate.txt")
+wet_diameter_evaporation = np.loadtxt("wet_diameters_evaporation.txt")
+wet_diameter_deactivation =  np.loadtxt("wet_diameters_deactivation.txt")
+wet_diameter_inertial = np.loadtxt("wet_diameters_inertial.txt")
+
+equilibrium_rhs_not_activate_grid_100 = np.loadtxt("equilib_rhs_not_activate_grid_100.txt")
+equilibrium_rhs_evaporation_grid_100  = np.loadtxt("equilib_rhs_evaporation_grid_100.txt")
+equilibrium_rhs_deactivation_grid_100 =  np.loadtxt("equilib_rhs_deactivation_grid_100.txt")
+equilibrium_rhs_inertial_grid_100 = np.loadtxt("equilib_rhs_inertial_grid_100.txt")
+
+equilibrium_rhs_not_activate_grid_130 = np.loadtxt("equilib_rhs_not_activate_grid_130.txt")
+equilibrium_rhs_evaporation_grid_130  = np.loadtxt("equilib_rhs_evaporation_grid_130.txt")
+equilibrium_rhs_deactivation_grid_130 =  np.loadtxt("equilib_rhs_deactivation_grid_130.txt")
+equilibrium_rhs_inertial_grid_130 = np.loadtxt("equilib_rhs_inertial_grid_130.txt")
+
+(figure, axes) = mpl_helper.make_fig()
+l1 = axes.plot(seconds, diam_not_activate)
+l2 = axes.plot(seconds, diam_evaporation)
+l3 = axes.plot(seconds, diam_deactivation)
+l4 = axes.plot(seconds, diam_inertial)
+axes.grid(True)
+axes.set_yscale("log")
+axes.set_xlabel(r"time in seconds ")
+axes.set_ylabel(r"wet diameter")
+axes.legend((l1, l2, l3, l4), ("not activate", "evaporation", "deactivation", "inertial"))
+figure.savefig("figs/diameters.pdf")
+
+(figure, axes) = mpl_helper.make_fig()
+axes.plot(seconds, rh_eq_not_activate)
+axes.plot(seconds, rh_eq_evaporation)
+axes.plot(seconds, rh_eq_deactivation)
+axes.plot(seconds, rh_eq_inertial)
+axes.grid(True)
+axes.set_xlabel(r"time in seconds ")
+axes.set_ylabel(r"equilib rh of particle")
+figure.savefig("figs/rh_eqs.pdf")
+
+(figure, axes) = mpl_helper.make_fig()
+l1=axes.plot(diam_not_activate*1e6, rh-rh_eq_not_activate)
+l2=axes.plot(diam_evaporation*1e6, rh-rh_eq_evaporation)
+l3=axes.plot(diam_deactivation*1e6, rh-rh_eq_deactivation)
+#axes.plot(diam_inertial*1e6, rh_eq_inertial)
+axes.grid(True)
+axes.set_xscale("log")
+axes.set_xlabel(r"diameter ")
+axes.set_ylabel(r"equilib rh of particle")
+axes.legend((l1, l2, l3), ("not activate", "evaporation", "deactivation"), loc='center left')
+figure.savefig("figs/d_rh_not_activate.pdf")
+
+(figure, axes) = mpl_helper.make_fig()
+l1=axes.plot(diam_not_activate[80:150]*1e6, rh[80:150])
+l2=axes.plot(wet_diameter_not_activate*1e6, equilibrium_rhs_not_activate_grid_100)
+l3=axes.plot(wet_diameter_not_activate*1e6, equilibrium_rhs_not_activate_grid_130)
+axes.grid(True)
+axes.set_xscale("log")
+axes.set_xlabel(r"diameter / $\rm \mu m$")
+axes.set_ylabel(r"rh")
+axes.set_xlim(0.1, 10)
+axes.set_ylim(1, 1.003)
+axes.legend((l1, l2, l3), ("env rh", "eq rh 100", "eq rh 130"), loc='center left')
+figure.savefig("figs/rh_versus_d_not_activate.pdf")
+
+(figure, axes) = mpl_helper.make_fig()
+l1=axes.plot(diam_evaporation[80:150]*1e6, rh[80:150])
+l2=axes.plot(wet_diameter_evaporation*1e6, equilibrium_rhs_evaporation_grid_100)
+l3=axes.plot(wet_diameter_evaporation*1e6, equilibrium_rhs_evaporation_grid_130)
+axes.grid(True)
+axes.set_xscale("log")
+axes.set_xlabel(r"diameter / $\rm \mu m$")
+axes.set_ylabel(r"rh")
+axes.set_xlim(0.1, 10)
+axes.set_ylim(1, 1.003)
+axes.legend((l1, l2, l3), ("env rh", "eq rh 100", "eq rh 130"), loc='center left')
+figure.savefig("figs/rh_versus_d_evaporation.pdf")
+
+(figure, axes) = mpl_helper.make_fig()
+l1=axes.plot(diam_deactivation[80:150]*1e6, rh[80:150])
+l2=axes.plot(wet_diameter_deactivation*1e6, equilibrium_rhs_deactivation_grid_100)
+l3=axes.plot(wet_diameter_deactivation*1e6, equilibrium_rhs_deactivation_grid_130)
+axes.grid(True)
+axes.set_xscale("log")
+axes.set_xlabel(r"diameter / $\rm \mu m$")
+axes.set_ylabel(r"rh")
+axes.set_xlim(0.1, 10)
+axes.set_ylim(1, 1.003)
+axes.legend((l1, l2, l3), ("env rh", "eq rh 100", "eq rh 130"), loc='center left')
+figure.savefig("figs/rh_versus_d_deactivation.pdf")
+
+(figure, axes) = mpl_helper.make_fig()
+l1=axes.plot(diam_inertial[80:150]*1e6, rh[80:150])
+l2=axes.plot(wet_diameter_inertial*1e6, equilibrium_rhs_inertial_grid_100)
+l3=axes.plot(wet_diameter_inertial*1e6, equilibrium_rhs_inertial_grid_130)
+axes.grid(True)
+axes.set_xscale("log")
+axes.set_xlabel(r"diameter / $\rm \mu m$")
+axes.set_ylabel(r"rh")
+axes.set_xlim(1, 50)
+axes.set_ylim(1, 1.003)
+axes.legend((l1, l2, l3), ("env rh", "eq rh 100", "eq rh 130"), loc='center left')
+figure.savefig("figs/rh_versus_d_inertial.pdf")
+
+
+(figure, axes_array) = mpl_helper.make_fig_array(2,1, share_x_axes=True)
+axes = axes_array[0][0]
+l1=axes.plot(seconds, diam_not_activate*1e6)
+l2=axes.plot(seconds, d_c_not_activate*1e6)
+axes.grid(True)
+axes.set_xlabel(r"time / s")
+axes.set_ylabel(r"diameter / $\rm \mu m$")
+axes.legend((l1, l2), ("wet diameter", "critical diameter"), loc='center right')
+
+axes = axes_array[1][0]
+l1=axes.plot(seconds, rh)
+l2=axes.plot(seconds, rh_c_not_activate)
+l3=axes.plot(seconds, rh_eq_not_activate)
+axes.grid(True)
+axes.set_ylim(1, 1.005)
+axes.set_ylabel(r"rh")
+axes.legend((l1, l2), ("env", "critical", "eq"), loc='center right')
+mpl_helper.remove_fig_array_axes(axes_array, remove_y_axes=False)
+figure.savefig("figs/rhs_diams_not_activate.pdf")
+
+(figure, axes_array) = mpl_helper.make_fig_array(2,1, share_x_axes=True)
+axes = axes_array[0][0]
+l1=axes.plot(seconds, diam_evaporation*1e6)
+l2=axes.plot(seconds, d_c_evaporation*1e6)
+axes.grid(True)
+axes.set_xlabel(r"time / s")
+axes.set_ylabel(r"diameter / $\rm \mu m$")
+axes.legend((l1, l2), ("wet diameter", "critical diameter"), loc='center right')
+
+axes = axes_array[1][0]
+l1=axes.plot(seconds, rh)
+l2=axes.plot(seconds, rh_c_evaporation)
+l3=axes.plot(seconds, rh_eq_evaporation)
+axes.grid(True)
+axes.set_ylabel(r"rh")
+axes.set_ylim(1, 1.005)
+axes.legend((l1, l2, l3), ("env", "critical", "eq"), loc='center right')
+mpl_helper.remove_fig_array_axes(axes_array, remove_y_axes=False)
+figure.savefig("figs/rhs_diams_evaporation.pdf")
+
+(figure, axes_array) = mpl_helper.make_fig_array(2,1, share_x_axes=True)
+axes = axes_array[0][0]
+l1=axes.plot(seconds, diam_deactivation*1e6)
+l2=axes.plot(seconds, d_c_deactivation*1e6)
+axes.grid(True)
+axes.set_xlabel(r"time / s")
+axes.set_ylabel(r"diameter / $\rm \mu m$")
+axes.legend((l1, l2), ("wet diameter", "critical diameter"), loc='center right')
+
+axes = axes_array[1][0]
+l1=axes.plot(seconds, rh)
+l2=axes.plot(seconds, rh_c_deactivation)
+l3=axes.plot(seconds, rh_eq_deactivation)
+axes.grid(True)
+axes.set_ylabel(r"rh")
+axes.set_ylim(1, 1.005)
+axes.legend((l1, l2, l3), ("env", "critical", "eq"), loc='center right')
+mpl_helper.remove_fig_array_axes(axes_array, remove_y_axes=False)
+figure.savefig("figs/rhs_diams_deactivation.pdf")
+
+(figure, axes_array) = mpl_helper.make_fig_array(2,1, share_x_axes=True)
+axes = axes_array[0][0]
+l1=axes.plot(seconds, diam_inertial*1e6)
+l2=axes.plot(seconds, d_c_inertial*1e6)
+axes.grid(True)
+axes.set_xlabel(r"time / s")
+axes.set_ylabel(r"diameter / $\rm \mu m$")
+axes.legend((l1, l2), ("wet diameter", "critical diameter"), loc='center right')
+
+axes = axes_array[1][0]
+l1=axes.plot(seconds, rh)
+l2=axes.plot(seconds, rh_c_inertial)
+l3=axes.plot(seconds, rh_eq_inertial)
+axes.grid(True)
+axes.set_ylabel(r"rh")
+axes.set_ylim(1, 1.005)
+axes.legend((l1, l2, l3), ("env", "critical", "eq"), loc='center right')
+mpl_helper.remove_fig_array_axes(axes_array, remove_y_axes=False)
+figure.savefig("figs/rhs_diams_inertial.pdf")
--- figures/7_condensation/kinetic_effects_process.py
+++ figures/7_condensation/kinetic_effects_process.py
@@ -3,25 +3,35 @@
 import scipy.io
 import sys
 import numpy as np
-import matplotlib
-matplotlib.use("PDF")
-import matplotlib.pyplot as plt
+
 sys.path.append("../../tool")
 import partmc
+import matplotlib
+import mpl_helper
+import config
+#matplotlib.use("PDF")
+#import matplotlib.pyplot as plt
 
-def make_plot(filename_in, dir_cloud, in_file_pattern):
+
+def make_plot(filename_in_100, filename_in_130, dir_cloud, in_file_pattern):
 
 ## calculate critical supersaturation for each particle
-    print filename_in
-    ncf = scipy.io.netcdf.netcdf_file(filename_in, 'r')
+    print filename_in_100
+    ncf = scipy.io.netcdf.netcdf_file(filename_in_100, 'r')
     particles = partmc.aero_particle_array_t(ncf)
-    env_state_plume = partmc.env_state_t(ncf)
+    particles.sort_by_id()
+    env_state_100s = partmc.env_state_t(ncf)
     ncf.close()
 
     dry_diameters = particles.dry_diameters()
-    s_crit = (particles.critical_rel_humids(env_state_plume) - 1)*100
-    d_crit = particles.critical_diameters(env_state_plume)
-    kappas = particles.kappas()
+
+    ncf = scipy.io.netcdf.netcdf_file(filename_in_130, 'r')
+    particles = partmc.aero_particle_array_t(ncf)
+    particles.sort_by_id()
+    env_state_130s = partmc.env_state_t(ncf)
+    ncf.close()
+
+    dry_diameters = particles.dry_diameters()
 
 ## calculate time series for RH and maximum RH that occurs
     print dir_cloud, in_file_pattern
@@ -34,12 +44,21 @@ def make_plot(filename_in, dir_cloud, in_file_pattern):
     maximum_ss = (max(rh) - 1) * 100.
     print 'maximum ss ', maximum_ss
     final_ss = (rh[-1] - 1) * 100.  # takes the last value of the time series
+    final_rh = rh[-1]
     print 'final ss ', final_ss
  
-## calculate time series of diameters of each particle
+## calculate time series for each particle
     print dir_cloud, in_file_pattern
     time_filename_list = partmc.get_time_filename_list(dir_cloud, in_file_pattern)
+    rh_c = np.zeros((len(dry_diameters),len(time_filename_list)))
+    final_rh_c = np.zeros(len(dry_diameters))
+    d_c = np.zeros((len(dry_diameters),len(time_filename_list)))
     d = np.zeros((len(dry_diameters),len(time_filename_list)))
+    rh_eq = np.zeros((len(dry_diameters),len(time_filename_list)))
+
+    kappas = np.zeros((len(dry_diameters),len(time_filename_list)))
+    dry_diam = np.zeros((len(dry_diameters),len(time_filename_list)))
+    ids = np.zeros((len(dry_diameters),len(time_filename_list)), dtype = int)
     seconds = np.zeros(len(time_filename_list))
     i_count = 0
 
@@ -47,61 +66,175 @@ def make_plot(filename_in, dir_cloud, in_file_pattern):
         print time, filename, key
         ncf = scipy.io.netcdf.netcdf_file(filename, 'r')
         particles = partmc.aero_particle_array_t(ncf)
+        particles.sort_by_id()
+        env_state = partmc.env_state_t(ncf)
         ncf.close()
-        wet_diameters = particles.diameters()
-        d[:,i_count] = wet_diameters
+        rh_c[:,i_count] = particles.critical_rel_humids(env_state)
+        d_c[:,i_count] = particles.critical_diameters(env_state)
+        d[:,i_count] = particles.diameters()
+        rh_eq[:,i_count] = particles.equilib_rel_humids(env_state)
+        kappas[:,i_count] = particles.kappas()
+        dry_diam[:,i_count] = particles.dry_diameters()
+
+        ids[:,i_count] = particles.ids
         seconds[i_count] = i_count
         i_count = i_count + 1
 
     print "diameter for particle 1 ", d[1,:]
- 
+    print "kappa for particle 1 ", kappas[1,:]
+
     d_max = d.max(axis = 1)
+    final_rh_c[:] = rh_c[:,-1]
 
     print "dmax ", d_max   
     
 ## find the particles in the four categories
-## 1. particles that do now activate
+## 1. particles that do not activate
 
-    not_activate = ((maximum_ss < s_crit) & (d_max < d_crit))
+#    not_activate = ((maximum_ss < s_crit) & (d_max < d_crit))
+
+    not_activate = np.logical_and(np.all(rh < rh_c, axis=1), np.all(d<d_c, axis=1))
+
     id_list_not_activate = particles.ids[not_activate]
     print "id_list_not_activate ", id_list_not_activate
     plot_id = id_list_not_activate[0]
     plot_index = partmc.find_nearest_index(particles.ids, plot_id)
     diam_not_activate = d[plot_index,:]
-  
-    dry_diameter = dry_diameters[plot_index]
-    kappa = kappas[plot_index]
-    wet_diameters = partmc.log_grid(min = diam_not_activate.min(), max = diam_not_activate.max(), n_bin=100).edges()
-    equilib_rhs = partmc.equilib_rel_humids(env_state_init,
-            kappa, dry_diameter, wet_diameters)
-     
-    print "diam_not_activate, rh ", diam_not_activate, rh
-    plt.clf()
-    plt.plot(diam_not_activate*1e6, rh)
-    plt.hold(True)
-    plt.plot(wet_diameters*1e6, equilib_rhs, 'r') 
-    fig = plt.gcf()
-    fig.savefig("not_activate.pdf")
+    rh_eq_not_activate = rh_eq[plot_index,:]
+    ids_not_activate = ids[plot_index,:]
+    rh_c_not_activate = rh_c[plot_index,:]
+    d_c_not_activate = d_c[plot_index,:]
 
+    dry_diam_not_activate = dry_diam[plot_index,0]
+    kappa_not_activate = kappas[plot_index,0]
+    wet_diameters_not_activate = partmc.log_grid(min=1.1*dry_diam_not_activate, max=100*dry_diam_not_activate, n_bin=100).edges()
+
+    equilib_rhs_not_activate_grid_100 = partmc.equilib_rel_humids(env_state_100s,
+            kappa_not_activate, dry_diam_not_activate, wet_diameters_not_activate)
+    equilib_rhs_not_activate_grid_130 = partmc.equilib_rel_humids(env_state_130s,
+            kappa_not_activate, dry_diam_not_activate, wet_diameters_not_activate)
 
 ## 2. evaporation type     
   
-    evaporation = ((maximum_ss > s_crit) & (d_max < d_crit))
+#    evaporation = ((maximum_ss > s_crit) & (d_max < d_crit) & (final_ss < s_crit))
+    evaporation = np.logical_and(np.logical_and(np.any(rh > rh_c, axis=1), np.all(d < d_c, axis=1)), (final_rh < final_rh_c))
+
     id_list_evaporation = particles.ids[evaporation]
     print "id_list_evaporation ", id_list_evaporation
+
+    plot_id = id_list_evaporation[0]
+    plot_index = partmc.find_nearest_index(particles.ids, plot_id)
+    diam_evaporation = d[plot_index,:]
+    ids_evaporation = ids[plot_index,:]
+    rh_eq_evaporation = rh_eq[plot_index,:]
+    rh_c_evaporation = rh_c[plot_index,:]
+    d_c_evaporation = d_c[plot_index,:]
+
+    dry_diam_evaporation = dry_diam[plot_index,0]
+    kappa_evaporation = kappas[plot_index,0]
+    wet_diameters_evaporation = partmc.log_grid(min=1.1*dry_diam_evaporation, max=100*dry_diam_evaporation, n_bin=100).edges()
+
+    equilib_rhs_evaporation_grid_100 = partmc.equilib_rel_humids(env_state_100s,
+            kappa_evaporation, dry_diam_evaporation, wet_diameters_evaporation)
+    equilib_rhs_evaporation_grid_130 = partmc.equilib_rel_humids(env_state_130s,
+            kappa_evaporation, dry_diam_evaporation, wet_diameters_evaporation)
 ## 3. deactivation type
 
-    deactivation = ((maximum_ss > s_crit) & (d_max > d_crit))
+#    deactivation = ((maximum_ss > s_crit) & (d_max > d_crit))
+    deactivation = np.logical_and(np.any(rh > rh_c, axis=1), np.any(d > d_c, axis=1))
+
     id_list_deactivation = particles.ids[deactivation]
     print "id_list_deactivation ", id_list_deactivation
-## 4. inertal type
 
-    inertial = ((maximum_ss > s_crit) & (d_max < d_crit))
+    plot_id = id_list_deactivation[0]
+    plot_index = partmc.find_nearest_index(particles.ids, plot_id)
+    diam_deactivation = d[plot_index,:]
+    ids_deactivation = ids[plot_index,:]
+    rh_eq_deactivation = rh_eq[plot_index,:]
+    rh_c_deactivation = rh_c[plot_index,:]
+    d_c_deactivation = d_c[plot_index,:]
+
+    dry_diam_deactivation = dry_diam[plot_index,0]
+    kappa_deactivation = kappas[plot_index,0]
+    wet_diameters_deactivation = partmc.log_grid(min=1.1*dry_diam_deactivation, max=100*dry_diam_deactivation, n_bin=100).edges()
+
+    equilib_rhs_deactivation_grid_100 = partmc.equilib_rel_humids(env_state_100s,
+            kappa_deactivation, dry_diam_deactivation, wet_diameters_deactivation)
+    equilib_rhs_deactivation_grid_130 = partmc.equilib_rel_humids(env_state_130s,
+            kappa_deactivation, dry_diam_deactivation, wet_diameters_deactivation)
+
+## 4. inertial type
+
+#    inertial = ((maximum_ss > s_crit) & (d_max < d_crit) & (final_ss > s_crit))
+    inertial = np.logical_and(np.logical_and(np.any(rh > rh_c, axis=1), np.all(d < d_c, axis=1)), (final_rh > final_rh_c))
+
     id_list_inertial = particles.ids[inertial] 
     print "id_list_inertial ", id_list_inertial
 
-filename_in = "/home/ching1/subversion/partmc/branches/joseph2/scenarios/3_condense/out/ver210_ref_13/cond_tenthou_13_ref_ver210_mem_10_0001_00000013.nc"
-dir_cloud = "/home/ching1/subversion/partmc/branches/joseph2/scenarios/3_condense/out/ver210_ref_13"
+    plot_id = id_list_inertial[0]
+    plot_index = partmc.find_nearest_index(particles.ids, plot_id)
+    diam_inertial = d[plot_index,:]
+    ids_inertial = ids[plot_index,:]
+    rh_eq_inertial = rh_eq[plot_index,:]
+    rh_c_inertial = rh_c[plot_index,:]
+    d_c_inertial = d_c[plot_index,:]
+
+    dry_diam_inertial = dry_diam[plot_index,0]
+    kappa_inertial = kappas[plot_index,0]
+    wet_diameters_inertial = partmc.log_grid(min=1.1*dry_diam_inertial, max=100*dry_diam_inertial, n_bin=100).edges()
+
+    equilib_rhs_inertial_grid_100 = partmc.equilib_rel_humids(env_state_100s,
+            kappa_inertial, dry_diam_inertial, wet_diameters_inertial)
+    equilib_rhs_inertial_grid_130 = partmc.equilib_rel_humids(env_state_130s,
+            kappa_inertial, dry_diam_inertial, wet_diameters_inertial)
+
+    np.savetxt("seconds.txt", seconds)
+    np.savetxt("rh.txt", rh)
+
+    np.savetxt("diam_not_activate.txt", diam_not_activate)
+    np.savetxt("diam_evaporation.txt", diam_evaporation)
+    np.savetxt("diam_deactivation.txt", diam_deactivation)
+    np.savetxt("diam_inertial.txt", diam_inertial)
+
+    np.savetxt("rh_eq_not_activate.txt", rh_eq_not_activate)
+    np.savetxt("rh_eq_evaporation.txt", rh_eq_evaporation)
+    np.savetxt("rh_eq_deactivation.txt", rh_eq_deactivation)
+    np.savetxt("rh_eq_inertial.txt", rh_eq_inertial)
+
+    np.savetxt("ids_not_activate.txt", ids_not_activate)
+    np.savetxt("ids_evaporation.txt", ids_evaporation)
+    np.savetxt("ids_deactivation.txt", ids_deactivation)
+    np.savetxt("ids_inertial.txt", ids_inertial)
+
+    np.savetxt("rh_c_not_activate.txt", rh_c_not_activate)
+    np.savetxt("rh_c_evaporation.txt", rh_c_evaporation)
+    np.savetxt("rh_c_deactivation.txt", rh_c_deactivation)
+    np.savetxt("rh_c_inertial.txt", rh_c_inertial)
+
+    np.savetxt("d_c_not_activate.txt", d_c_not_activate)
+    np.savetxt("d_c_evaporation.txt", d_c_evaporation)
+    np.savetxt("d_c_deactivation.txt", d_c_deactivation)
+    np.savetxt("d_c_inertial.txt", d_c_inertial)
+
+    np.savetxt("wet_diameters_not_activate.txt", wet_diameters_not_activate)
+    np.savetxt("wet_diameters_evaporation.txt", wet_diameters_evaporation)
+    np.savetxt("wet_diameters_deactivation.txt", wet_diameters_deactivation)
+    np.savetxt("wet_diameters_inertial.txt", wet_diameters_inertial)
+
+    np.savetxt("equilib_rhs_not_activate_grid_100.txt", equilib_rhs_not_activate_grid_100)
+    np.savetxt("equilib_rhs_evaporation_grid_100.txt", equilib_rhs_evaporation_grid_100)
+    np.savetxt("equilib_rhs_deactivation_grid_100.txt", equilib_rhs_deactivation_grid_100)
+    np.savetxt("equilib_rhs_inertial_grid_100.txt", equilib_rhs_inertial_grid_100)
+
+    np.savetxt("equilib_rhs_not_activate_grid_130.txt", equilib_rhs_not_activate_grid_130)
+    np.savetxt("equilib_rhs_evaporation_grid_130.txt", equilib_rhs_evaporation_grid_130)
+    np.savetxt("equilib_rhs_deactivation_grid_130.txt", equilib_rhs_deactivation_grid_130)
+    np.savetxt("equilib_rhs_inertial_grid_130.txt", equilib_rhs_inertial_grid_130)
+
+
+filename_in_100 = "condensation_data/cond_tenthou_13_ref_ver210_mem_10_0001_00000101.nc"
+filename_in_130 = "condensation_data/cond_tenthou_13_ref_ver210_mem_10_0001_00000131.nc"
+dir_cloud = "condensation_data"
 in_file_pattern = "cond_tenthou_13_ref_ver210_mem_10_0001_.*.nc"
 
-make_plot(filename_in, dir_cloud, in_file_pattern)
+make_plot(filename_in_100, filename_in_130, dir_cloud, in_file_pattern)

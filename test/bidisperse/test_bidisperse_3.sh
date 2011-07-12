27681be690b16c901a196fb2a8b4ada47727c476
Matthew West <mwest@illinois.edu>
2011-07-12 06:57:12 +0000

    relax tolerance in test_bidisperse_3

--- test/bidisperse/test_bidisperse_3.sh
+++ test/bidisperse/test_bidisperse_3.sh
@@ -7,4 +7,4 @@ set -v
 # make sure that the current directory is the one where this script is
 cd ${0%/*}
 
-../../numeric_diff out/bidisperse_part_data.txt out/bidisperse_ode_data.txt 0 5e-2 0 0 3 3
+../../numeric_diff out/bidisperse_part_data.txt out/bidisperse_ode_data.txt 0 0.12 0 0 3 3

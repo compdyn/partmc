bff41ec4a02b0cfaafafdf01b181b68e4b4cec71
Matthew West <mwest@illinois.edu>
2012-03-25 16:58:58 -0500

    show 1e-3/0.5 ratios in plot_errors

--- scenarios/10_coag_conv_multi_brownian/5_plot_errors.py
+++ scenarios/10_coag_conv_multi_brownian/5_plot_errors.py
@@ -4,6 +4,7 @@ import numpy
 import sys, os
 sys.path.append("../../tool")
 import mpl_helper
+import matplotlib
 import config
 
 colors = ['b', 'r', 'g']
@@ -13,6 +14,9 @@ shapes = ['x', '.', '+']
 
 handles = []
 labels = []
+saved_shapes = []
+shape_handles = []
+shape_labels = []
 for (i_part, (n_part, n_part_name, n_part_tex)) in enumerate(config.n_part_list):
     line_x = []
     err_x = []
@@ -42,8 +46,31 @@ for (i_part, (n_part, n_part_name, n_part_tex)) in enumerate(config.n_part_list)
         err_x.append(num_1_err_ci)
         err_y.append(num_2_err_ci)
 
-    (plotline, caplines, barlinecols) = axes.errorbar(line_x, line_y, err_y, err_x,
-                                                      fmt=(colors[i_part]))
+        plot_extra = False
+        if ratio == "0.999":
+            plot_extra = True
+            shape = 'o'
+            shape_labels.append("equal weight")
+        if ratio == "0.5":
+            plot_extra = True
+            shape = 's'
+            shape_labels.append("equal number")
+        if plot_extra:
+            plotline = axes.plot([num_1_err_mean], [num_2_err_mean],
+                                 color=colors[i_part], marker=shape, markerfacecolor='w',
+                                 markeredgecolor=colors[i_part], markersize=8, markeredgewidth=1,
+                                 linestyle='None')
+            if shape not in saved_shapes:
+                saved_shapes.append(shape)
+                shape_handles.append(matplotlib.lines.Line2D([], [],
+                                                             color=colors[i_part], marker=shape, markerfacecolor='w',
+                                                             markeredgecolor='k', markersize=8, markeredgewidth=1,
+                                                             linestyle='None'))
+                #shape_handles.append(plotline)
+
+    #(plotline, caplines, barlinecols) = axes.errorbar(line_x, line_y, err_y, err_x,
+    #                                                  fmt=(colors[i_part] + ".-"))
+    plotline = axes.plot(line_x, line_y, colors[i_part] + ".-")
     handles.append(plotline)
     labels.append("%s" % n_part_tex)
 
@@ -51,7 +78,8 @@ axes.set_xscale('log')
 axes.set_yscale('log')
 axes.set_xlabel(r'mean number 1 error $E[\|n_1 - n_{1, \rm s}\|_2]$')
 axes.set_ylabel(r'mean number 2 error $E[\|n_2 - n_{2, \rm s}\|_2]$')
-figure.legend(handles, labels, loc='center right', numpoints=1)
+figure.legend(handles, labels, loc='upper right', numpoints=3)
+figure.legend(shape_handles, shape_labels, loc='lower right', numpoints=1)
 axes.grid(True)
 
 figure.savefig(os.path.join(config.fig_dirname, "errors.pdf"))

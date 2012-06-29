9e8bd5af7ee3514cb12ff3631813bd58594e9c51
Jian Tian <jtian4@illinois.edu>
2012-06-29 16:08:20 -0500

    add df = 2 SPSD free molecular case, add naumann SPSD testing cases

---
+++ test/fractal/test_fractal_09.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# exit on error
+set -e
+# turn on command echoing
+set -v
+# make sure that the current directory is the one where this script is
+cd ${0%/*}
+# make the output directory if it doesn't exist
+mkdir -p out
+
+../../partmc run_part_naumann_free_df_3.spec
+
+../../test_fractal_self_preserve --num --dimless_vol_min 1e-3 --dimless_vol_max 10 --nbin 100 out/part_naumann_free_df_3_0001
+
+../../numeric_diff --by col --rel-tol 0.1 ref_free_df_3_self_preserve_regrid.txt out/part_naumann_free_df_3_0001_self_preserve.txt

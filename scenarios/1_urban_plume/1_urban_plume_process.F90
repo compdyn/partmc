4fcb860bf244939354047899efd72edfad9872e6
Matthew West <mwest@illinois.edu>
2012-08-20 19:14:16 -0500

    rearrange 1_urban_plume_process for readability

--- scenarios/1_urban_plume/1_urban_plume_process.F90
+++ scenarios/1_urban_plume/1_urban_plume_process.F90
@@ -26,11 +26,11 @@ program process
   real(kind=dp), allocatable :: times(:), dry_diameters(:), num_concs(:), &
        dry_masses(:), masses(:), bc_masses(:), bc_fracs(:), &
        num_concs_averaged(:), dry_masses_averaged(:), masses_averaged(:), &
-       entropies(:), entropies_averaged(:), crit_rhs(:), scs(:)
-  type(stats_1d_t) :: stats_num_dist, stats_entropy_dist
+       entropies(:), entropies_averaged(:), crit_rhs(:), scs(:), num_dist(:), &
+       diam_bc_dist(:,:), diam_sc_dist(:,:), entropy_dist(:)
+  type(stats_1d_t) :: stats_num_dist, stats_entropy_dist, stats_tot_num_conc, &
+       stats_tot_mass_conc, stats_tot_entropy, stats_tot_entropy_averaged
   type(stats_2d_t) :: stats_diam_bc_dist, stats_diam_sc_dist
-  type(stats_1d_t) :: stats_tot_num_conc, stats_tot_mass_conc, &
-       stats_tot_entropy, stats_tot_entropy_averaged
 
   call pmc_mpi_init()
 
@@ -58,30 +58,51 @@ program process
      do i_repeat = 1,n_repeat
         call make_filename(in_filename, prefix, ".nc", i_index, i_repeat)
         write(*,*) "Processing " // trim(in_filename)
+        ! FIXME: add UUID check into input_state(), keyed off of index or
+        ! time or something? Or init to "" and check if not this.
         call input_state(in_filename, index, time, del_t, repeat, &
              uuid, aero_data=aero_data, aero_state=aero_state, &
              env_state=env_state)
 
-        ! FIXME: add UUID check into input_state(), keyed off of index or
-        ! time or something? Or init to "" and check if not this.
+        times(i_index) = time
 
         dry_diameters = aero_state_dry_diameters(aero_state, aero_data)
         num_concs = aero_state_num_concs(aero_state)
+        num_dist = bin_grid_histogram_1d(diam_grid, dry_diameters, num_concs)
+        call stats_1d_add(stats_num_dist, num_dist)
+
+        tot_num_conc = sum(num_concs)
+        call stats_1d_add_entry(stats_tot_num_conc, tot_num_conc, i_index)
+
         masses = aero_state_masses(aero_state, aero_data)
+        tot_mass_conc = sum(masses * num_concs)
+        call stats_1d_add_entry(stats_tot_mass_conc, tot_mass_conc, i_index)
+
         dry_masses = aero_state_masses(aero_state, aero_data, &
              exclude=(/"H2O"/))
         bc_masses = aero_state_masses(aero_state, aero_data, &
              include=(/"BC"/))
+        bc_fracs = bc_masses / dry_masses
+        diam_bc_dist = bin_grid_histogram_2d(diam_grid, dry_diameters, &
+             bc_grid, bc_fracs, num_concs)
+        call stats_2d_add(stats_diam_bc_dist, diam_bc_dist)
+
         crit_rhs = aero_state_crit_rel_humids(aero_state, aero_data, &
              env_state)
         scs = crit_rhs - 1d0
-        bc_fracs = bc_masses / dry_masses
-        tot_num_conc = sum(num_concs)
-        tot_mass_conc = sum(masses * num_concs)
+        diam_sc_dist = bin_grid_histogram_2d(diam_grid, dry_diameters, &
+             sc_grid, scs, num_concs)
+        call stats_2d_add(stats_diam_sc_dist, diam_sc_dist)
+
         entropies = aero_state_mass_entropies(aero_state, aero_data) !, &
              !exclude=["H2O"]) !, group=["BC"])
+        entropy_dist = bin_grid_histogram_1d(entropy_grid, entropies, &
+             num_concs)
+        call stats_1d_add(stats_entropy_dist, entropy_dist)
+
         tot_entropy = sum(entropies * masses * num_concs) &
              / sum(masses * num_concs)
+        call stats_1d_add_entry(stats_tot_entropy, tot_entropy, i_index)
 
         call aero_state_copy(aero_state, aero_state_averaged)
         call aero_state_bin_average_comp(aero_state_averaged, avg_bin_grid, &
@@ -96,24 +117,8 @@ program process
              = sum(entropies_averaged * masses_averaged &
              * num_concs_averaged) &
              / sum(masses_averaged * num_concs_averaged)
-
-        times(i_index) = time
-        call stats_1d_add_entry(stats_tot_num_conc, tot_num_conc, i_index)
-        call stats_1d_add_entry(stats_tot_mass_conc, tot_mass_conc, i_index)
-        call stats_1d_add_entry(stats_tot_entropy, tot_entropy, i_index)
         call stats_1d_add_entry(stats_tot_entropy_averaged, &
              tot_entropy_averaged, i_index)
-        call stats_1d_add(stats_num_dist, &
-             bin_grid_histogram_1d(diam_grid, dry_diameters, num_concs))
-        call stats_2d_add(stats_diam_bc_dist, &
-             bin_grid_histogram_2d(diam_grid, dry_diameters, &
-             bc_grid, bc_fracs, num_concs))
-        call stats_2d_add(stats_diam_sc_dist, &
-             bin_grid_histogram_2d(diam_grid, dry_diameters, &
-             sc_grid, scs, num_concs))
-        call stats_1d_add(stats_entropy_dist, &
-             bin_grid_histogram_1d(entropy_grid, entropies, &
-             num_concs))
      end do
 
      call make_filename(out_filename, prefix, "_process.nc", index)
@@ -123,20 +128,24 @@ program process
      call bin_grid_output_netcdf(bc_grid, ncid, "bc_frac", unit="1")
      call bin_grid_output_netcdf(sc_grid, ncid, "sc", unit="1")
      call bin_grid_output_netcdf(entropy_grid, ncid, "entropy", unit="1")
+
      call stats_1d_output_netcdf(stats_num_dist, ncid, "num_dist", &
           dim_name="diam", unit="m^{-3}")
+     call stats_1d_clear(stats_num_dist)
+
      call stats_2d_output_netcdf(stats_diam_bc_dist, ncid, "diam_bc_dist", &
           dim_name_1="diam", dim_name_2="bc_frac", unit="m^{-3}")
+     call stats_2d_clear(stats_diam_bc_dist)
+
      call stats_2d_output_netcdf(stats_diam_sc_dist, ncid, "diam_sc_dist", &
           dim_name_1="diam", dim_name_2="sc", unit="m^{-3}")
+     call stats_2d_clear(stats_diam_sc_dist)
+
      call stats_1d_output_netcdf(stats_entropy_dist, ncid, "entropy_dist", &
           dim_name="entropy", unit="m^{-3}")
-     call pmc_nc_close(ncid)
-
-     call stats_1d_clear(stats_num_dist)
-     call stats_2d_clear(stats_diam_bc_dist)
-     call stats_2d_clear(stats_diam_sc_dist)
      call stats_1d_clear(stats_entropy_dist)
+
+     call pmc_nc_close(ncid)
   end do
 
   call make_filename(out_filename, prefix, "_process.nc")

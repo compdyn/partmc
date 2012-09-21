afa9bb9e9dcb308a04a434a45e9671c399332818
Matthew Michelotti <mmichel3@illinois.edu>
2012-09-21 14:15:22 -0500

    debugging 1-D removal

--- src/scenario.F90
+++ src/scenario.F90
@@ -667,6 +667,10 @@ contains
     integer :: c, b, init_size, candidates, cand_iter, s, p, check_size
     real(kind=dp) :: over_rate, rate, vol, density
     
+    print *, 'A'
+    call aero_state_check_sort(aero_state)
+    print *, 'B'
+    
     if(function_id == SCENARIO_LOSS_FUNCTION_ZERO .or. &
         function_id == SCENARIO_LOSS_FUNCTION_INVALID) return
     
@@ -701,6 +705,10 @@ contains
               // trim(real_to_string(over_rate)) )
           if (pmc_random() * over_rate > rate) cycle
           
+          if(p < 1 .or. p > aero_state%apa%n_part) then
+            print *, "invalid particle id:", p, aero_state%apa%n_part
+          end if
+          
           call aero_info_allocate(aero_info)
           aero_info%id = aero_particle%id
           aero_info%action = AERO_INFO_DILUTION
@@ -715,6 +723,10 @@ contains
         end do
       end do
     end do
+    
+    print *, 'C'
+    call aero_state_check_sort(aero_state)
+    print *, 'D'
     
 !    do b = 1,aero_state%aero_sorted%bin_grid%n_bin
 !      over_rate = aero_state%aero_sorted%removal_rate_max(b)

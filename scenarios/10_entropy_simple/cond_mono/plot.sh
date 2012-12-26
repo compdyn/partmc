680b40574a3e46b1380fa4b00adf1c3c8316a70e
Nicole Riemer <nriemer@illinois.edu>
2012-12-26 12:51:25 -0600

    added cond_mono case

---
+++ scenarios/10_entropy_simple/cond_mono/plot.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+./plot_aero_ccn.py
+./plot_aero_diam_bc_dist.py
+./plot_aero_diam_entropy_dist.py
+./plot_aero_total.py
+./plot_aero_diam_entropy_scatter.py
+./plot_aero_diam_sc_dist.py
+#./plot_aero_entropy_conc.py 
+#./plot_aero_particle_entropy.py 
+#./plot_aero_entropy_dist.py 
+#./plot_aero_dist_ratio_bar.py 
+#./plot_aero_dist_ratio_hat.py
+

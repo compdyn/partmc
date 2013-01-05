60168409ed5121da5afcbf279a47418dedb3f398
Nicole Riemer <nriemer@illinois.edu>
2013-01-05 17:25:27 -0600

    modified emit_mixed case

--- scenarios/10_entropy_simple/emit_mixed/plot_aero_particle_entropy.py
+++ scenarios/10_entropy_simple/emit_mixed/plot_aero_particle_entropy.py
@@ -6,7 +6,6 @@ import mpl_helper
 import scipy.io, numpy
 
 (figure, axes) = mpl_helper.make_fig(right_margin=0.8)
-axes2 = axes.twinx()
 
 ncf = scipy.io.netcdf_file("out/urban_plume_process.nc")
 time = ncf.variables["time"].data / 3600
@@ -17,13 +16,28 @@ entropy_of_avg_part = ncf.variables["entropy_of_avg_part"].data
 entropy_of_avg_part_err = ncf.variables["entropy_of_avg_part_ci_offset"].data
 tot_entropy_ratio = ncf.variables["tot_entropy_ratio"].data
 
-axes.errorbar(time, avg_part_entropy, avg_part_entropy_err, fmt="r-", label = r'$\bar{H}$')
-axes.errorbar(time, entropy_of_avg_part, entropy_of_avg_part_err, fmt="g-", label = r'$\bar{\hat{H}}$')
-axes2.plot(time, tot_entropy_ratio, "m-", label = r'$R$')
+axes.plot(time, avg_part_entropy, "b-", label = r'$\bar{H}$')
+axes.plot(time, entropy_of_avg_part, "k:", label = r'$\bar{\hat{H}}$')
+axes.plot(time, tot_entropy_ratio, "r--", markersize = 2, label = r'$\gamma$')
 axes.set_xlabel(r"time / h")
-axes.set_ylabel(r"entropy")
-axes2.set_ylabel(r"entropy ratio")
-axes.legend(loc = 'lower right')
+axes.set_ylabel(r"quantity")
+axes.set_xlim([0,24])
+axes.set_xticks([0, 6, 12, 18, 24])
+
+axes.annotate(r"$\bar{H}$", (time[7], avg_part_entropy[7]),
+              verticalalignment="bottom", horizontalalignment="right",
+              bbox = dict(facecolor='white', edgecolor='white'),
+              xytext=(0, 5), textcoords='offset points')
+axes.annotate(r"$\bar{\hat{H}}$", (time[11], entropy_of_avg_part[11]),
+              verticalalignment="bottom", horizontalalignment="right",
+              bbox = dict(facecolor='white', edgecolor='white'),
+              xytext=(0, 5), textcoords='offset points')
+axes.annotate(r"$\gamma$", (time[17], tot_entropy_ratio[17]),
+              verticalalignment="bottom", horizontalalignment="right",
+              bbox = dict(facecolor='white', edgecolor='white'),
+              xytext=(0, 5), textcoords='offset points')
+
+#axes.legend(loc = 'lower right')
 axes.grid(True)
 
 figure.savefig("out/urban_plume_particle_entropy.pdf")

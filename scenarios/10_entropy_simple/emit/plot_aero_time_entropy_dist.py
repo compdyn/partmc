accda96e2b9b2e1e897bbf0b885762195dc67502
Nicole Riemer <nriemer@illinois.edu>
2013-01-19 14:43:49 -0600

    added plot_aero_time_entropy_dist.py in the cases directories

---
+++ scenarios/10_entropy_simple/emit/plot_aero_time_entropy_dist.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys, os
+sys.path.append("../../../tool")
+import mpl_helper
+import matplotlib
+import partmc
+import scipy.io, numpy
+
+filename = 'out/urban_plume_process.nc'
+(figure, axes, cbar_axes) = mpl_helper.make_fig(left_margin=0.7, right_margin=1, colorbar=True)
+
+ncf = scipy.io.netcdf_file(filename)
+time_grid_edges = ncf.variables["time_grid_edges"].data
+entropy_edges = ncf.variables["entropy_edges"].data
+time_entropy_dist = ncf.variables["time_entropy_dist"].data
+
+p = axes.pcolor(time_grid_edges, entropy_edges, time_entropy_dist.transpose(),
+                norm = matplotlib.colors.LogNorm(), linewidths = 0.1)
+
+axes.set_xscale("linear")
+axes.set_xlabel(r"time / h")
+axes.set_xlim(0, 24e0)
+axes.set_xticks([0, 6, 12, 18, 24])
+
+axes.set_yscale("linear")
+axes.set_ylabel(r"particle entropy $H_i$")
+axes.set_ylim(0, 1.1)
+
+axes.grid(True)
+cbar = figure.colorbar(p, cax=cbar_axes, format=matplotlib.ticker.LogFormatterMathtext(),
+                       orientation='vertical')
+cbar_axes.xaxis.set_label_position('top')
+cbar.set_label(r"number conc. $n(t, H_i)$ / $\rm m^{-3}$")
+
+out_filename = "out/urban_plume_time_entropy_emit.pdf"
+figure.savefig(out_filename)
+print out_filename

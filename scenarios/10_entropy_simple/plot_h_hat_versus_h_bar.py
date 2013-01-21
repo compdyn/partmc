992296240304ff1c2ed9c5fa3766c0db3ed7a660
Nicole Riemer <nriemer@illinois.edu>
2013-01-21 15:34:15 -0600

    modified plotting script

--- scenarios/10_entropy_simple/plot_h_hat_versus_h_bar.py
+++ scenarios/10_entropy_simple/plot_h_hat_versus_h_bar.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot
 from matplotlib.patches import Polygon
 from matplotlib.patches import FancyArrowPatch
 
-(figure, axes) = mpl_helper.make_fig(figure_width=5)
+(figure, axes) = mpl_helper.make_fig(figure_width=5, axis_ratio=1)
 
 ncf = scipy.io.netcdf_file("emit/out/urban_plume_process.nc")
 avg_part_entropy1 = ncf.variables["avg_part_entropy"].data
@@ -51,82 +51,97 @@ avg_part_entropy8 = ncf.variables["avg_part_entropy"].data
 entropy_of_avg_part8 = ncf.variables["entropy_of_avg_part"].data
 ncf.close()
 
-print avg_part_entropy5[0], avg_part_entropy5[1], entropy_of_avg_part5[0], entropy_of_avg_part5[1]
-axes.plot(avg_part_entropy1/1.1, entropy_of_avg_part1/1.1, "b-", linewidth=2)
+axes.plot(avg_part_entropy1[0:73]/1.1, entropy_of_avg_part1[0:73]/1.1, "b-", linewidth=2)
 axes.plot(avg_part_entropy2/1.4, entropy_of_avg_part2/1.4, "b--", linewidth=2)
 axes.plot(avg_part_entropy3/1.1, entropy_of_avg_part3/1.1, "c--", linewidth=2)
-axes.plot(avg_part_entropy4/0.7, entropy_of_avg_part4/0.7, "r-", linewidth=2)
+axes.plot(avg_part_entropy4/np.log(2), entropy_of_avg_part4/np.log(2), "r-", linewidth=2)
 axes.plot(avg_part_entropy5/1.4, entropy_of_avg_part5/1.4, "k-", linewidth=2)
 axes.plot(avg_part_entropy6/1.4, entropy_of_avg_part6/1.4, "k--", linewidth=2)
 axes.plot(avg_part_entropy7/1.4, entropy_of_avg_part7/1.4, "g-", linewidth=2)
 axes.plot(avg_part_entropy8/1.4, entropy_of_avg_part8/1.4, "g--", linewidth=2)
 
+x_values = np.linspace(0,2,21)
+
+axes.plot(x_values, x_values*1, "k", linewidth=0.5)
+axes.plot(x_values, x_values*2, "k", linewidth=0.5)
+
+axes.annotate(r"$\gamma=0.5$", (x_values[5], 2*x_values[5]),
+              verticalalignment="bottom", horizontalalignment="right",
+              bbox = dict(facecolor='white', edgecolor='white'),
+              xytext=(0, 5), textcoords='offset points')
+
+axes.annotate(r"$\gamma=1$", (x_values[10], x_values[10]),
+              verticalalignment="bottom", horizontalalignment="right",
+              bbox = dict(facecolor='white', edgecolor='white'),
+              xytext=(0, 5), textcoords='offset points')
+
+
 axes.set_xlabel(r"$\bar{H}/\ln(A)$")
-axes.set_xlim([0,1])
+axes.set_xlim([0,1.1])
 axes.set_xticks([0, 0.2, 0.4, 0.6, 0.8, 1.0])
-axes.set_ylim([0,1.2])
-axes.set_yticks([0, 0.2, 0.4, 0.6, 0.8, 1, 1.2])
-axes.set_ylabel(r"$\bar{\hat{H}}/\ln{A}$")
+axes.set_ylim([0,1.1])
+axes.set_yticks([0, 0.2, 0.4, 0.6, 0.8, 1])
+axes.set_ylabel(r"$\bar{\hat{H}}/\ln(A)$")
 pts = np.array([[0,0], [1,1], [0,1]])
 p = Polygon(pts,closed=True,alpha=0.05)
 axes.add_patch(p)
 
-#axes.annotate("",
-#            xy=(avg_part_entropy1[9]/1.1,entropy_of_avg_part1[9]), xycoords='data',
-#            xytext=(avg_part_entropy1[5]/1.1,entropy_of_avg_part1[5]), textcoords='data',
-#            arrowprops=dict(arrowstyle="simple, head_width=0.5, head_length=1, tail_width=0.001",
-#                            connectionstyle="arc3", ec ="b", fc = "b", ),
-#            )
+axes.annotate("",
+            xy=(avg_part_entropy1[45]/1.1,entropy_of_avg_part1[45]/1.1), xycoords='data',
+            xytext=(avg_part_entropy1[25]/1.1,entropy_of_avg_part1[25]/1.1), textcoords='data',
+            arrowprops=dict(arrowstyle="simple, head_width=0.5, head_length=1, tail_width=0.001",
+                            connectionstyle="arc3", ec ="b", fc = "b", ),
+            )
 
-#axes.annotate("",
-#            xy=(avg_part_entropy2[12]/1.4,entropy_of_avg_part2[12]), xycoords='data',
-#            xytext=(avg_part_entropy2[5]/1.4,entropy_of_avg_part2[5]), textcoords='data',
-#            arrowprops=dict(arrowstyle="simple, head_width=0.5, head_length=1, tail_width=0.001",
-#                            connectionstyle="arc3", ec ="b", fc = "b", ),
-#            )
+axes.annotate("",
+            xy=(avg_part_entropy2[6]/1.4,entropy_of_avg_part2[6]/1.4), xycoords='data',
+            xytext=(avg_part_entropy2[3]/1.4,entropy_of_avg_part2[3]/1.4), textcoords='data',
+            arrowprops=dict(arrowstyle="simple, head_width=0.5, head_length=1, tail_width=0.001",
+                            connectionstyle="arc3", ec ="b", fc = "b", ),
+            )
 
-#axes.annotate("",
-#            xy=(avg_part_entropy3[15]/1.1,entropy_of_avg_part3[15]), xycoords='data',
-#            xytext=(avg_part_entropy3[5]/1.1,entropy_of_avg_part3[5]), textcoords='data',
-#            arrowprops=dict(arrowstyle="simple, head_width=0.5, head_length=1, tail_width=0.001",
-#                            connectionstyle="arc3", ec ="c", fc = "c", ),
-#            )
+axes.annotate("",
+            xy=(avg_part_entropy3[10]/1.1,entropy_of_avg_part3[10]/1.1), xycoords='data',
+            xytext=(avg_part_entropy3[5]/1.1,entropy_of_avg_part3[5]/1.1), textcoords='data',
+            arrowprops=dict(arrowstyle="simple, head_width=0.5, head_length=1, tail_width=0.001",
+                            connectionstyle="arc3", ec ="c", fc = "c", ),
+            )
 
-#axes.annotate("",
-#            xy=(avg_part_entropy4[15]/0.7,entropy_of_avg_part4[15]), xycoords='data',
-#            xytext=(avg_part_entropy4[5]/0.7,entropy_of_avg_part4[5]), textcoords='data',
-#            arrowprops=dict(arrowstyle="simple, head_width=0.5, head_length=1, tail_width=0.001",
-#                            connectionstyle="arc3", ec ="r", fc = "r", ),
-#            )
+axes.annotate("",
+            xy=(avg_part_entropy4[15]/np.log(2),entropy_of_avg_part4[15]/np.log(2)), xycoords='data',
+            xytext=(avg_part_entropy4[5]/np.log(2),entropy_of_avg_part4[5]/np.log(2)), textcoords='data',
+            arrowprops=dict(arrowstyle="simple, head_width=0.5, head_length=1, tail_width=0.001",
+                            connectionstyle="arc3", ec ="r", fc = "r", ),
+            )
 
-#axes.annotate("",
-#            xy=(avg_part_entropy5[7]/1.4,entropy_of_avg_part5[7]), xycoords='data',
-#            xytext=(avg_part_entropy5[5]/1.4,entropy_of_avg_part5[5]), textcoords='data',
-#            arrowprops=dict(arrowstyle="simple, head_width=0.5, head_length=1, tail_width=0.001",
-#                            connectionstyle="arc3", ec ="k", fc = "k", ),
-#            )
+axes.annotate("",
+            xy=(avg_part_entropy5[7]/1.4,entropy_of_avg_part5[7]/1.4), xycoords='data',
+            xytext=(avg_part_entropy5[5]/1.4,entropy_of_avg_part5[5]/1.4), textcoords='data',
+            arrowprops=dict(arrowstyle="simple, head_width=0.5, head_length=1, tail_width=0.001",
+                            connectionstyle="arc3", ec ="k", fc = "k", ),
+            )
 
-#axes.annotate("",
-#            xy=(avg_part_entropy6[17]/1.4,entropy_of_avg_part6[17]), xycoords='data',
-#            xytext=(avg_part_entropy6[5]/1.4,entropy_of_avg_part6[5]), textcoords='data',
-#            arrowprops=dict(arrowstyle="simple, head_width=0.5, head_length=1, tail_width=0.001",
-#                            connectionstyle="arc3", ec ="k", fc = "k", ),
-#            )
+axes.annotate("",
+            xy=(avg_part_entropy6[8]/1.4,entropy_of_avg_part6[8]/1.4), xycoords='data',
+            xytext=(avg_part_entropy6[5]/1.4,entropy_of_avg_part6[5]/1.4), textcoords='data',
+            arrowprops=dict(arrowstyle="simple, head_width=0.5, head_length=1, tail_width=0.001",
+                            connectionstyle="arc3", ec ="k", fc = "k", ),
+            )
 
-#axes.annotate("",
-#            xy=(avg_part_entropy7[16]/1.4,entropy_of_avg_part7[16]), xycoords='data',
-#            xytext=(avg_part_entropy7[6]/1.4,entropy_of_avg_part7[6]), textcoords='data',
-#            arrowprops=dict(arrowstyle="simple, head_width=0.5, head_length=1, tail_width=0.001",
-#                            connectionstyle="arc3", ec ="g", fc = "g", ),
-#            )
+axes.annotate("",
+            xy=(avg_part_entropy7[16]/1.4,entropy_of_avg_part7[16]/1.4), xycoords='data',
+            xytext=(avg_part_entropy7[6]/1.4,entropy_of_avg_part7[6]/1.4), textcoords='data',
+            arrowprops=dict(arrowstyle="simple, head_width=0.5, head_length=1, tail_width=0.001",
+                            connectionstyle="arc3", ec ="g", fc = "g", ),
+            )
 
-#print 'here'
-#axes.annotate("",
-#            xy=(avg_part_entropy8[17]/1.4,entropy_of_avg_part8[17]), xycoords='data',
-#            xytext=(avg_part_entropy8[8]/1.4,entropy_of_avg_part8[8]), textcoords='data',
-#            arrowprops=dict(arrowstyle="simple, head_width=0.5, head_length=1, tail_width=0.0001",
-#                            connectionstyle="arc3", ec ="g", fc = "g", ),
-#            )
+print 'here'
+axes.annotate("",
+            xy=(avg_part_entropy8[15]/1.4,entropy_of_avg_part8[15]/1.4), xycoords='data',
+            xytext=(avg_part_entropy8[8]/1.4,entropy_of_avg_part8[8]/1.4), textcoords='data',
+            arrowprops=dict(arrowstyle="simple, head_width=0.5, head_length=1, tail_width=0.0001",
+                            connectionstyle="arc3", ec ="g", fc = "g", ),
+            )
 
 axes.grid(True)
 figure.savefig("h_hat_versus_h_bar.pdf")

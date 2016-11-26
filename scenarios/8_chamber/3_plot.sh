ced5ff08ff4fca6fbe44fd46a77cd99b79b41da4
Matthew West <mwest@illinois.edu>
2016-11-25 22:22:20 -0600

    remove mass information from scenarios/8_chamber as conversions with mob_diam are wrong

--- scenarios/8_chamber/3_plot.sh
+++ scenarios/8_chamber/3_plot.sh
@@ -7,7 +7,5 @@ set -v
 
 # The data should have already been processed by ./process.sh
 
-gnuplot -persist plot_size_mass.gnuplot
 gnuplot -persist plot_size_num.gnuplot
-gnuplot -persist plot_time_mass.gnuplot
 gnuplot -persist plot_time_num.gnuplot

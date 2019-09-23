e203cdeb5488ea247e4dc74f5ec4e1ee6884d319
Matt Dawson <mattldawson@gmail.com>
2019-09-23 16:41:15 -0700

    fix species index in check for empty cells

--- src/camp_solver.c
+++ src/camp_solver.c
@@ -874,10 +874,11 @@ int camp_solver_update_model_state(N_Vector solver_state,
   int n_dep_var   = model_data->n_per_cell_dep_var;
   int n_cells     = model_data->n_cells;
 
+  int i_dep_var = 0;
   for (int i_cell=0; i_cell<n_cells; i_cell++) {
-    for (int i_spec=0, i_dep_var=0; i_spec<n_state_var; ++i_spec) {
+    for (int i_spec=0; i_spec<n_state_var; ++i_spec) {
       if (model_data->var_type[i_spec]==CHEM_SPEC_VARIABLE) {
-        if (NV_DATA_S(solver_state)[i_cell*n_dep_var+i_dep_var] < -SMALL) {
+        if (NV_DATA_S(solver_state)[i_dep_var] < -SMALL) {
 #ifdef FAILURE_DETAIL
           printf("\nFailed model state update: [spec %d] = %le", i_spec,
               NV_DATA_S(solver_state)[i_dep_var]);
@@ -886,8 +887,8 @@ int camp_solver_update_model_state(N_Vector solver_state,
         }
         //Assign model state to solver_state
         model_data->total_state[i_spec+i_cell*n_state_var] =
-          NV_DATA_S(solver_state)[i_dep_var+i_cell*n_dep_var] > threshhold ?
-          NV_DATA_S(solver_state)[i_dep_var+i_cell*n_dep_var] : replacement_value;
+          NV_DATA_S(solver_state)[i_dep_var] > threshhold ?
+          NV_DATA_S(solver_state)[i_dep_var] : replacement_value;
         i_dep_var++;
       }
     }
@@ -1991,14 +1992,16 @@ bool is_anything_going_on_here(SolverData *sd, realtype t_initial, realtype t_fi
   ModelData *md = &(sd->model_data);
 
   if( f(t_initial, sd->y, sd->deriv, sd) ) {
-    for (int i_spec=0, i_dep_var=0;
-         i_spec < md->n_per_cell_state_var * md->n_cells; i_spec++) {
-      if (md->var_type[i_spec]==CHEM_SPEC_VARIABLE) {
-        if( NV_Ith_S(sd->y, i_dep_var) >
-            NV_Ith_S(sd->abs_tol_nv, i_dep_var) * 1.0e-10 ) return true;
-        if( NV_Ith_S(sd->deriv, i_dep_var) * ( t_final - t_initial ) >
-            NV_Ith_S(sd->abs_tol_nv, i_dep_var) * 1.0e-10 ) return true;
-        i_dep_var++;
+    int i_dep_var = 0;
+    for (int i_cell=0; i_cell<md->n_cells; ++i_cell) {
+      for (int i_spec=0; i_spec < md->n_per_cell_state_var; ++i_spec) {
+        if (md->var_type[i_spec]==CHEM_SPEC_VARIABLE) {
+          if( NV_Ith_S(sd->y, i_dep_var) >
+              NV_Ith_S(sd->abs_tol_nv, i_dep_var) * 1.0e-10 ) return true;
+          if( NV_Ith_S(sd->deriv, i_dep_var) * ( t_final - t_initial ) >
+              NV_Ith_S(sd->abs_tol_nv, i_dep_var) * 1.0e-10 ) return true;
+          i_dep_var++;
+        }
       }
     }
     return false;

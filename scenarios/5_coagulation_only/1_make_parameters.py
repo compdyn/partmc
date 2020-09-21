b419a4b30a5e5a0ccbac27fdf27f68e758e09f61
Jeff Curtis <jcurtis2@illinois.edu>
2020-09-21 15:57:44 -0500

    updated sampling from linear to log for diameter and concentrations

--- scenarios/5_coagulation_only/1_make_parameters.py
+++ scenarios/5_coagulation_only/1_make_parameters.py
@@ -10,16 +10,19 @@ n_modes = 3
 lhs_min = np.zeros((3*3))
 lhs_max = np.zeros((3*3))
 for ii in range(3):
-   lhs_min[ii] = diam_range[0]
-   lhs_max[ii] = diam_range[1]
+   lhs_min[ii] = np.log10(diam_range[0])
+   lhs_max[ii] = np.log10(diam_range[1])
    lhs_min[ii + n_modes] = sigma_range[0]
    lhs_max[ii + n_modes] = sigma_range[1]
-   lhs_min[ii + 2*n_modes] = conc_range[0]
-   lhs_max[ii + 2*n_modes] = conc_range[1]
+   lhs_min[ii + 2*n_modes] = np.log10(conc_range[0])
+   lhs_max[ii + 2*n_modes] = np.log10(conc_range[1])
 
 print(lhs_min)
 print(lhs_max) 
 lhs_prob = pyDOE.lhs(len(lhs_min), n_scenarios)
 lhs = lhs_min + (lhs_max-lhs_min) * lhs_prob
+
+lhs[:,0:3] = 10**lhs[:,0:3]
+lhs[:,6:] = 10**lhs[:,6:]
 
 np.savetxt('lhs.txt', lhs)

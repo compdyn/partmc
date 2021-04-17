18fb88e7e0891d3b73acd1358c6d4ba6f153f795
Matthew West <mwest@illinois.edu>
2021-04-17 10:24:06 -0500

    Add first version of test/mixing_state

---
+++ test/mixing_state/test_mixing_state_1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# exit on error
+set -e
+# turn on command echoing
+set -v
+# make sure that the current directory is the one where this script is
+cd ${0%/*}
+# make the output directory if it doesn't exist
+mkdir -p out
+
+../../partmc run_part.spec
+../../test_mixing_state_process
+
+../../numeric_diff --by elem --rel-tol 1e-3 ref_all_species.txt out/test_all_species.txt

fad476c3e5082b4ef478bb5b8440a6467a96f3c2
Jeff Curtis <jcurtis2@illinois.edu>
2023-12-12 21:21:39 -0600

    improve weighitng tests. add plotting

--- test/weighting/test_weighting_4.sh
+++ test/weighting/test_weighting_4.sh
@@ -11,6 +11,6 @@ mkdir -p out
 
 ../../partmc run_part_nummass.spec
 
-../../extract_aero_size --num --dmin 1e-8 --dmax 1e-3 --nbin 160 out/weighting_flat_0001
+../../extract_aero_size --num --dmin 1e-8 --dmax 1e-3 --nbin 160 out/weighting_nummass_0001
 ../../extract_sectional_aero_size --num out/weighting_exact
-../../numeric_diff --by col --rel-tol 0.1 out/weighting_exact_aero_size_num.txt out/weighting_flat_0001_aero_size_num.txt
+../../numeric_diff --by col --rel-tol 0.1 out/weighting_exact_aero_size_num.txt out/weighting_nummass_0001_aero_size_num.txt

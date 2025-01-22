95d8f8d8f0a38266ea1ff786d23f2bf75d2aee16
Wenhan Tang <81504538+tangwhiap@users.noreply.github.com>
2025-01-22 13:59:16 -0600

    Update src/env_state.F90
    Co-authored-by: Jeffrey Curtis <jcurtis2@illinois.edu>

--- src/env_state.F90
+++ src/env_state.F90
@@ -650,7 +650,6 @@ contains
   !> Formula (10) from [Murphy & Koop, 2004]  (https://doi.org/10.1256/qj.04.94)
   real(kind=dp) function env_state_saturated_vapor_pressure_water(T)
 
-
     !> temperature (k)
     real(kind=dp), intent(in) :: T
 

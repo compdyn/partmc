00234bb536e6e0635aefa5562b141ff66b60c289
tangwhiap <wenhant2@illinois.edu>
2025-09-14 18:39:48 -0500

    Simplify the freezing scenario, keep only exp8

--- scenarios/7_freezing/1_run.sh
+++ scenarios/7_freezing/1_run.sh
@@ -16,18 +16,14 @@ cp -rp $setDir $outDir/
 # Copy the constant settings among experiments
 cp -p $setDir/*.dat .
 cp -p $setDir/run_part.spec .
-for expName in exp1 exp2 exp3 exp4 exp5 exp6 exp7 exp8
-do
-    caseName=${expName}
-    echo "Running $caseName ..."
+echo "Running $caseName ..."
 # Copy the special settings for that experiment
-    cp -p $setDir/$expName/*.dat .
-    sed -i "/output_prefix /coutput_prefix ${outDir}/${caseName}/freezing_part # prefix of output files" run_part.spec
-    mkdir -p $outDir/$caseName
-    cp -p run_part.spec $outDir/$caseName
-    cp -p *.dat $outDir/$caseName
-    sleep 1
+cp -p $setDir/$expName/*.dat .
+sed -i "/output_prefix /coutput_prefix ${outDir}/freezing_part # prefix of output files" run_part.spec
+mkdir -p $outDir
+cp -p run_part.spec $outDir
+cp -p *.dat $outDir
+sleep 1
 
-    ../../build/partmc run_part.spec
+../../build/partmc run_part.spec
     
-done

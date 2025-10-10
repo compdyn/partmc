6b136043cbc3f05a75e7e566d4b17176f0960e3a
tangwhiap <wenhant2@illinois.edu>
2025-10-10 09:06:10 -0500

    Stop tracking 7_freezing scenario

--- scenarios/7_freezing/2_process.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-N_ensemble=10
-
-for i_ensemble in $(seq 1 $N_ensemble); do
-	prefix=$(printf "freezing_part_%04d " $i_ensemble)
-	../../build/freezing_process out/$prefix
-done
-    
--- scenarios/7_freezing/4_clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -f *.dat
-rm -fr out
-rm -f run_part.spec

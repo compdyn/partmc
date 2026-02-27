2cc950a2dc3bf84d09a8f1a5c8d54f2c5f14a0af
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2026-02-27 18:01:12 +0100

    add melt test

--- test/freezing/test_freezing_4.sh
+++ test/freezing/test_freezing_4.sh
@@ -16,8 +16,5 @@ mkdir -p out/ABIFM_naive
 # extract frozen fraction results to a txt file
 ../../test_freezing_extract out/ABIFM_naive/freezing_part
 
-# calculate the freezing theoretical results
-#../../test_freezing_theoretical ABIFM
-
 # compare the PartMC versus theoretical results
 ../../numeric_diff --rel-tol 0.02 out/ABIFM_naive/freezing_part_frozen_fraction_ensemble_mean.txt out/freezing_theoretical_ABIFM_data.txt
--- test/freezing/test_freezing_5.sh
+++ test/freezing/test_freezing_5.sh
@@ -16,8 +16,5 @@ mkdir -p out/const_naive
 # extract frozen fraction results to a txt file
 ../../test_freezing_extract out/const_naive/freezing_part
 
-# calculate the freezing theoretical results
-#../../test_freezing_theoretical const
-
 # compare the PartMC versus theoretical results
 ../../numeric_diff --rel-tol 0.02 out/const_naive/freezing_part_frozen_fraction_ensemble_mean.txt out/freezing_theoretical_const_data.txt
